aad829f0529646a3e140c68dc2427ce69c753a1c
Dilshod <br.dilshod@gmail.com>
2017-08-24 17:39:10 +0500

    345

--- frontend/widgets/lists/Models.php
+++ frontend/widgets/lists/Models.php
@@ -13,7 +13,7 @@ class Models extends Widget {
     public $brand = '';
 
     public function run() {
-        $sql = 'select id, title, url, image from {{%pages}} where parent =:parent and type =:type and active = 1 order by sort';
+        $sql = 'select id, title, url, image from {{%pages}} where parent =:parent and type =:type and active = 1 order by title';
         $rows = \Yii::$app->db->createCommand($sql)->bindValues(['parent' => $this->parent, 'type' => $this->type])->queryAll();
         $sortedRows = [];
         $searches = [];
