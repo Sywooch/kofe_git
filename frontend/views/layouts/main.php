0628039f9d19c5336e768dd0527ea14633bf0807
Abdulaziz <youngmaster.web@gmail.com>
2017-08-24 12:03:33 +0500

    ertertert

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -13,7 +13,8 @@ $isBrandPage = Yii::$app->controller->id == 'list' && Yii::$app->controller->act
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
+    <head>  
+    
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
