e4098bddc6176f6839f5a254bb7db1f950c051d8
Dilshod <br.dilshod@gmail.com>
2017-08-25 19:24:12 +0500

    werwer

--- frontend/components/CController.php
+++ frontend/components/CController.php
@@ -22,23 +22,23 @@ class CController extends \yii\web\Controller {
 //        self::$menu = $rows;
 //        unset($rows);
         //Yii::$app->ipgeobase->updateDB();
-//        $userIP = Yii::$app->getRequest()->getUserIP();
-//        $userRegionInfo = Yii::$app->ipgeobase->getLocation($userIP, true);
-//        if (empty(Yii::$app->session['region'])) {
-//            $regions = Yii::$app->params['regions'];
-//            if (!empty($userRegionInfo->city)) {
-//                if (isset($userRegionInfo->city)) {
-//                    foreach ($regions as $region) {
-//                        if (stripos($region['title'], $userRegionInfo->city) !== false) {
-//                            $this->setRegion($region['id']);
-//                            break;
-//                        }
-//                    }
-//                }
-//            } else {
-//                $this->setRegion(1);
-//            }
-//        }
+        $userIP = Yii::$app->getRequest()->getUserIP();
+        $userRegionInfo = Yii::$app->ipgeobase->getLocation($userIP, true);
+        if (empty(Yii::$app->session['region'])) {
+            $regions = Yii::$app->params['regions'];
+            if (!empty($userRegionInfo->city)) {
+                if (isset($userRegionInfo->city)) {
+                    foreach ($regions as $region) {
+                        if (stripos($region['title'], $userRegionInfo->city) !== false) {
+                            $this->setRegion($region['id']);
+                            break;
+                        }
+                    }
+                }
+            } else {
+                $this->setRegion(1);
+            }
+        }
         if (empty(Yii::$app->session['region']))
             $this->setRegion(1);
         if (isset($_GET['region'])) {
