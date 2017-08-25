6f4741888f4e9b887da25686c8392da4b59fce87
Abdulaziz <youngmaster.web@gmail.com>
2017-08-25 17:06:22 +0500

    service

--- frontend/views/list/service.php
+++ frontend/views/list/service.php
@@ -39,9 +39,6 @@ $this->title = $title;
 
                     <div class="clear"></div>
 
-                    <h3>Консультация и заказ</h3>
-                    <?= \app\widgets\forms\CallBack::widget(['metrika' => 'service-order-button']); ?>
-
                     <span>Специалист перезвонит в течение 5 минут.</span>
                 </div>
                 <div class="clear"></div>
