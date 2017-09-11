86859e34027c766cfda5a28bd56cf7ecf86a0784
Dilshod <br.dilshod@gmail.com>
2017-09-11 16:23:30 +0500

    werwer

--- frontend/views/contact/index.php
+++ frontend/views/contact/index.php
@@ -3,6 +3,7 @@ $breadcrumbs = [
     $pageInfo['title'],
 ];
 $this->title = $pageInfo['meta_title'];
+$siteConfig = app\components\CController::getSiteConfig();
 ?>
 <div class="clear"></div>
 <?= \app\widgets\other\Breadcrumbs::widget(['data' => $breadcrumbs]); ?>
@@ -16,35 +17,37 @@ $this->title = $pageInfo['meta_title'];
             <p>Мы, сервисный центр по ремонту кофемашин &laquo;РемонтКофе&raquo; - команда профессионалов, которые знают все о кофемашинах от А до Я. Наши мастера работают как на выезде, так и в сервисном центре, осуществляя ремонт кофемашин любой сложности.</p>
 
             <div class="clear">&nbsp;</div>
-            <div class="msk-contact all-contact<?= Yii::$app->session['region']['id'] == 1 ? ' active' : ''; ?>">
-                <h2>Наш адрес в <div class="will-active-m"></div><span class="v-msk active">Москве</span> <span class="v-spb">Санкт-Петербурге</span></h2>
-                <p>
-                    Почта: <a href="mailto:zakaz@remontkofe.ru">zakaz@remontkofe.ru</a><br />
-                    Номер телефона: <span class="moskva">8 (499)450-90-08</span><br />
-                    Работаем без выходных с 08:00 до 22:00<br />
-                    Центральный офис:&nbsp;м Фили, м. Шелепиха ул. Заречная 1к2, вход со двора
-                </p>
-                <div class="map">
-                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A491aa95e1bc192d0a1b6997ca799239d6ddde59426eb4d6d87042832d6436424&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=false"></script>
+            <?php if (!isset($siteConfig['spb-multi'])): ?>
+                <div class="msk-contact all-contact<?= Yii::$app->session['region']['id'] == 1 ? ' active' : ''; ?>">
+                    <h2>Наш адрес в <div class="will-active-m"></div><span class="v-msk active">Москве</span> <span class="v-spb">Санкт-Петербурге</span></h2>
+                    <p>
+                        Почта: <a href="mailto:zakaz@remontkofe.ru">zakaz@remontkofe.ru</a><br />
+                        Номер телефона: <span class="moskva">8 (499)450-90-08</span><br />
+                        Работаем без выходных с 08:00 до 22:00<br />
+                        Центральный офис:&nbsp;м Фили, м. Шелепиха ул. Заречная 1к2, вход со двора
+                    </p>
+                    <div class="map">
+                        <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A491aa95e1bc192d0a1b6997ca799239d6ddde59426eb4d6d87042832d6436424&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=false"></script>
+                    </div>
+                    <?php if (!empty($children) && 3 == 1): ?>
+                        <section id="links">
+                            <div class="container">
+                                <p class="title"><span><?= $pageInfo['icon'] == 'rayon' ? 'Метро' : 'Районы' ?> </span></p>
+                            </div>
+                            <div class="container">
+                                <ul>
+                                    <?php foreach ($children as $child): ?>
+                                        <li><a href="/<?= $child['url']; ?>"><?= $child['title']; ?></a></li>
+                                    <?php endforeach; ?>
+                                    <div class="clear"></div>
+                                </ul>
+                            </div>
+                        </section>
+                    <?php endif; ?>
                 </div>
-                <?php if (!empty($children) && 3 == 1): ?>
-                    <section id="links">
-                        <div class="container">
-                            <p class="title"><span><?= $pageInfo['icon'] == 'rayon' ? 'Метро' : 'Районы' ?> </span></p>
-                        </div>
-                        <div class="container">
-                            <ul>
-                                <?php foreach ($children as $child): ?>
-                                    <li><a href="/<?= $child['url']; ?>"><?= $child['title']; ?></a></li>
-                                <?php endforeach; ?>
-                                <div class="clear"></div>
-                            </ul>
-                        </div>
-                    </section>
-                <?php endif; ?>
-            </div>
+            <?php endif; ?>
             <div class="spb-contact all-contact<?= Yii::$app->session['region']['id'] == 2 ? ' active' : ''; ?>">
-                <h2>Наш адрес в <div class="will-active-m"></div><span class="v-spb active">Санкт-Петербурге</span> <span class="v-msk">Москве</span> </h2>
+                <h2>Наш адрес в <div class="will-active-m"></div><span class="v-spb active">Санкт-Петербурге</span> <?php if (!isset($siteConfig['spb-multi'])): ?><span class="v-msk">Москве</span><?php endif; ?> </h2>
                 <p>
                     Почта: <a href="mailto:zakaz@remontkofe.ru">zakaz@remontkofe.ru</a><br />
                     Номер телефона: <span class="spb">8 (812) 643-21-07</span><br />
