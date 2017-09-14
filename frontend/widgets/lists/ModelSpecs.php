6a785f24750a01b68a990ccb78708065ae645924
Dilshod <br.dilshod@gmail.com>
2017-09-14 16:31:47 +0500

    werwer

--- frontend/widgets/lists/ModelSpecs.php
+++ frontend/widgets/lists/ModelSpecs.php
@@ -3,15 +3,20 @@
 namespace app\widgets\lists;
 
 use yii\base\Widget;
-use yii\helpers\Html;
+use Yii;
 
 class ModelSpecs extends Widget {
 
     public $modelId = null;
 
     public function run() {
-        
-        return $this->render('model-specs', ['specs' => $specs]);
+        $sql = 'SELECT * FROM {{%specs}} WHERE model_id = ' . (int) $this->modelId . ' ORDER BY spec_group desc';
+        $specs = Yii::$app->db->createCommand($sql)->queryAll();
+        $features = [];
+        foreach ($specs as $spec) {
+            $features[$spec['spec_group']][] = $spec;
+        }       
+        return $this->render('model-specs', ['specs' => $features]);
     }
 
 }
