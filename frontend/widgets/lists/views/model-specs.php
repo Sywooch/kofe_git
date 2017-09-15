dd35ef1f1019e247893ff39ec7ee36f4d40ebae9
Abdulaziz <youngmaster.web@gmail.com>
2017-09-15 12:15:32 +0500

    brendi 10sht

--- frontend/widgets/lists/views/model-specs.php
+++ frontend/widgets/lists/views/model-specs.php
@@ -1,15 +1,24 @@
-<div>
-    <?php foreach ($specs as $gr => $features): ?>
-        <h2><?= $gr; ?></h2>
-        <?php foreach ($features as $feature): ?>
-            <dl>
-                <dt>
-                    <span><?= $feature['spec_name']; ?></span>
-                </dt>
-                <dd>
-                    <span><?= $feature['spec_value']; ?></span>
-                </dd>
-            </dl>
-        <?php endforeach; ?>
-    <?php endforeach; ?>
-</div>
+<section id="tseni" class="options">
+    <div class="container">
+        <div>
+            <div class="rochi-uslugi">
+                <?php foreach ($specs as $gr => $features): ?>
+                    <div class="harakteristiki">
+                        <h3><?= $gr; ?></h3>
+                            <table class="pricelist">
+                            <tbody>
+                            <?php foreach ($features as $feature): ?>
+                                <tr>
+                                    <td class="name"><?= $feature['spec_name']; ?></td>
+                                    <td class="op"><?= $feature['spec_value']; ?></td>
+                                </tr>
+                            <?php endforeach; ?>
+                            </tbody>
+                        </table>
+                    </div>
+                <?php endforeach; ?>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</section>
