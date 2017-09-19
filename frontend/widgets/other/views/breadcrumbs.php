e5dca55d215eab709f20a11a215c60892fa9dac7
Dilshod <br.dilshod@gmail.com>
2017-09-19 12:24:25 +0500

    sdfsdf

--- frontend/widgets/other/views/breadcrumbs.php
+++ frontend/widgets/other/views/breadcrumbs.php
@@ -4,24 +4,38 @@ $brand = app\components\CController::$monoBrand;
 ?>
 <section id="hlebniye-kroshki">
     <div class="container">
-        <ul>
+        <ul itemscope itemtype="http://schema.org/BreadcrumbList">
             <?php if ($siteConfig['mono']): ?>
-                <li><a href="/">Ремонт кофемашин <?= $brand['title']; ?></a></li>
+                <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                    <a itemscope itemtype="http://schema.org/Thing" itemprop="item"  href="/">Ремонт кофемашин <?= $brand['title']; ?></a>
+                    <meta itemprop="position" content="0" />
+                </li>
             <?php else: ?>
-                <li><a href="/">Ремонт кофемашин</a></li>
+                <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                    <a itemscope itemtype="http://schema.org/Thing" itemprop="item"  href="/">Ремонт кофемашин</a>
+                    <meta itemprop="position" content="0" />
+                </li>
             <?php endif; ?>
+            <?php $a = 1; ?>
             <?php foreach ($data as $url => $title): ?>
                 <?php
                 if ($siteConfig['mono'])
                     $url = str_replace(\app\components\CController::$monoBrand['url'] . '/', Yii::$app->params['replace-url'], $url);
                 ?>
                 <?php if (end($data) == $title): ?>                
-                    <li><?= $title; ?></li>
+                    <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                        <?= $title; ?>
+                        <meta itemprop="position" content="<?= $a; ?>" />
+                    </li>
                 <?php else: ?>
                     <?php if ('/' . $brand['url'] != $url): ?>
-                        <li><a href="<?= $url; ?>"><?= $title; ?></a></li>
+                        <li itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem">
+                            <a itemscope itemtype="http://schema.org/Thing" itemprop="item"  href="<?= $url; ?>"><?= $title; ?></a>
+                            <meta itemprop="position" content="<?= $a; ?>" />
+                        </li>
+                        <?php $a++; ?>
                     <?php endif; ?>
-                <?php endif; ?>
+                <?php endif; ?>                
             <?php endforeach; ?>
         </ul>
     </div>
