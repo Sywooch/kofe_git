9afe4c0c1fa62a0d1f43bee0a0139cb6a52a5a07
Dilshod <br.dilshod@gmail.com>
2017-09-19 16:52:44 +0500

    ertert

--- frontend/components/MainUrlRule.php
+++ frontend/components/MainUrlRule.php
@@ -71,8 +71,8 @@ class MainUrlRule extends UrlRule {
                 ->where(['url' => Yii::$app->request->pathInfo, 'site_id' => $siteConfig['id']])
                 ->limit(1)
                 ->one();
-        $sql = 'select * from {{%pages}} where lower(url) =:url limit 1';
-        $page = Yii::$app->db->createCommand($sql)->bindValues(['url' => $url])->queryOne();
+        $sql = 'select * from {{%pages}} where lower(url) =:url AND site_id =:id limit 1';
+        $page = Yii::$app->db->createCommand($sql)->bindValues(['url' => $url, 'id' => $siteConfig['id']])->queryOne();
         if (!empty($seo)) {
             $page['meta_key'] = $seo['meta_keywords'];
             $page['meta_desc'] = $seo['meta_description'];
