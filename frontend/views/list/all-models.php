a2bb2656cef5ceeced21fc93bd18fdc3a65e164f
Dilshod <br.dilshod@gmail.com>
2017-09-19 12:03:21 +0500

    ertert

--- frontend/views/list/all-models.php
+++ frontend/views/list/all-models.php
@@ -3,19 +3,18 @@ $siteConfig = app\components\CController::getSiteConfig();
 $this->title = $seo['meta_title'];
 $h1 = !empty($seo['meta_h1']) ? $seo['meta_h1'] : 'Ремонтируем модели ' .  \app\components\CController::$monoBrand['title'] . ':';
 $text = !empty($seo['meta_text1']) ? $seo['meta_text1'] : 'Опытные мастера и большой склад запчастей позволяют ремонтировать все популярные модели кофемашин.
-                Если у вас есть вопросы, то оставляйте онлайн-заявку, наши специалисты по работе с клиентами свяжутся с вами в течение 15 минут. 
-                Если хотите обратиться вне очереди, звоните по тел. ';
+                Если у вас есть вопросы, то оставляйте онлайн-заявку, наши специалисты по работе с клиентами свяжутся с вами в течение 15 минут.';
 ?>
 <section id="banner">
     <div class="container">
         <div class="full-text">
             <div class="h1">                
                 <h1><?= $h1; ?></h1>
-            </div>            
-            <p>
+            </div>           
+           
                 <?= $text; ?>                
-            </p>
-            <a class="phone moskva" href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= Yii::$app->session['region']['phone']; ?></a>
+            
+            <p>Если хотите обратиться вне очереди, звоните по тел. <a class="phone moskva" href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= Yii::$app->session['region']['phone']; ?></a></p>
         </div>
     </div>
     <div class="clear"></div>
