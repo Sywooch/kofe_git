d3c88b24c9f0f9763069deabe268e0c34b2d46f9
Dilshod <br.dilshod@gmail.com>
2017-09-20 18:06:49 +0500

    ertert

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -30,6 +30,7 @@ if ($siteConfig['mono'])
         <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
         <!--[if lte IE 7]><script src="js/IE8.js" type="text/javascript"></script><![endif]-->
         <!--[if lt IE 7]><link rel="stylesheet" type="text/css" media="all" href="css/ie6.css"/><![endif]-->
+        <style>#banner h2, #banner h3 {font-family: "NeuronExtraBold",cursive;clear: both;padding-top: 15px;margin: 0px;}#banner p {font-size: 15px;float: left;clear: both;margin-bottom: 15px;}</style>
     </head>    
     <body id="index" class="home<?= $isHome ? ' video' : ''; ?><?= $isModelPage || $isBrandPage ? ' banners' : ''; ?><?= $isModelPage ? ' model-page' : ''; ?>">
 
