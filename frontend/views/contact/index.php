713eae0d1f02988f3047cbf49de312c7ed5ddb51
Dilshod <br.dilshod@gmail.com>
2017-09-20 17:44:33 +0500

    werwer

--- frontend/views/contact/index.php
+++ frontend/views/contact/index.php
@@ -35,7 +35,7 @@ $siteConfig = app\components\CController::getSiteConfig();
                     <p>                        
                         Номер телефона: <span class="moskva"><?= Yii::$app->session['region']['phone']; ?></span><br />
                         Работаем без выходных с 08:00 до 22:00<br />
-                        Центральный офис: м. Садовая, Московский проспект 2/6, подезд 6, этаж 3
+                        Центральный офис: м. Сенная площадь, м. Спасская, ул Ефимова 2, ТЦ ПИК, этаж 1, павильон 100
                     </p>
                     <div class="map">
                         <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3Aef863b008884b6b061a3182be8f2ee3235ee72e0176c43a2c9681fa7d00a56c5&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
