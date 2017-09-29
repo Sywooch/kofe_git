16c4c06681a9175bfa65c9ccf2aa8c5cfafb2b25
Dilshod <br.dilshod@gmail.com>
2017-09-29 15:10:48 +0500

    ertert

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -151,7 +151,7 @@ $js = app\components\CController::$js;
             }#ask2 .container p {font-size: 24px;} #banner h2, #banner h3 {font-family: "NeuronExtraBold",cursive;clear: both;padding-top: 15px;margin: 0px;}#banner p {font-size: 15px;float: none;clear: both;margin-bottom: 15px;}</style>
     </head>    
     <body id="index" class="home <?= $siteConfig['sitePrefix']; ?><?= $isHome ? ' video' : ''; ?><?= $isModelPage || $isBrandPage ? ' banners' : ''; ?><?= $isModelPage ? ' model-page' : ''; ?>">
-<img src="<?= $assets ?>/<?= $siteConfig['sitePrefix']; ?>images/<?= $siteConfig['sitePrefix']; ?>coffee_machine.png" alt="">
+        <img style="opacity: 0;position: absolute;z-index: -999999999;" src="<?= $assets ?>/<?= $siteConfig['sitePrefix']; ?>images/<?= $siteConfig['sitePrefix']; ?>coffee_machine.png" alt="">
         <?php $this->beginBody() ?>
         <?php if ($isHome): ?>
             <div class="bg-all">
