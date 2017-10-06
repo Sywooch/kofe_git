d86eb33478843434adfd462cbdbe2a25c99111ac
Dilshod <br.dilshod@gmail.com>
2017-10-06 14:46:25 +0500

    ertert

--- frontend/widgets/forms/views/ask.php
+++ frontend/widgets/forms/views/ask.php
@@ -68,7 +68,7 @@ $form = ActiveForm::begin([
                 </ul>            
                 <a href="/kontakty">Наши Контакты</a>
             <?php endif; ?>
-            <div class="tel"><a class="phone moskva" href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= Yii::$app->session['region']['phone']; ?></a></div>
+            <div class="tel"><a class="phone <?= Yii::$app->session['region']['class']; ?>" href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= Yii::$app->session['region']['phone']; ?></a></div>
         </div>
         <div class="clear"></div>
     </div>
