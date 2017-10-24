51d2305eef2f150c207b6064d43edf791ba91058
Dilshod <br.dilshod@gmail.com>
2017-10-24 15:57:11 +0500

    wer

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -26,7 +26,7 @@ $js = app\components\CController::$js;
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
-        <link rel="icon" type="image/x-icon" href="<?= $assets ?>/<?= $siteConfig['sitePrefix']; ?>images/<?= $siteConfig['sitePrefix']; ?>favicon.ico">
+        <link rel="icon" type="image/x-icon" href="<?= $assets ?>/favicon.ico">
 
 <!--[if IE]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
 <!--[if lte IE 7]><script src="js/IE8.js" type="text/javascript"></script><![endif]-->
