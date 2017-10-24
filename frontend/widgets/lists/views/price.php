4b9b4ba16a62cea10979707dcd6be8d314e573ef
Dilshod <br.dilshod@gmail.com>
2017-10-24 17:10:50 +0500

    345345

--- frontend/widgets/lists/views/price.php
+++ frontend/widgets/lists/views/price.php
@@ -29,28 +29,37 @@
                     </tbody>
                 </table>
             </div>
+            <?php
+            $neis = [];
+            ?>
             <?php if (!$brandPage): ?>
                 <div class="right">
                     <h3>Цены по неисправностям</h3>
                     <table class="pricelist">
                         <tbody>
                             <?php foreach ($neispravnosti as $neispravnost): ?>
-                                <tr>
-                                    <td class="name">
-                                        <a href="/<?= !empty($urlPrefix) ? $urlPrefix . '/' : ''; ?><?= $neispravnost['url']; ?>"><?= $neispravnost['title']; ?></a>
-                                    </td>
-                                    <td class="price">от <?= round($neispravnost['price']); ?>&nbsp;р</td>
-                                    <td class="order">
-                                        <div class="popup-zakaz">Заказать</div>
-                                    </td>
-                                </tr>
+                                <?php
+                                if ($neispravnost['is_popular'] != 1) {
+                                    $neis[] = $neispravnost;
+                                }
+                                ?>
+                                <?php if ($neispravnost['is_popular'] == 1): ?>
+                                    <tr>
+                                        <td class="name">
+                                            <a href="/<?= !empty($urlPrefix) ? $urlPrefix . '/' : ''; ?><?= $neispravnost['url']; ?>"><?= $neispravnost['title']; ?></a>
+                                        </td>
+                                        <td class="price">от <?= round($neispravnost['price']); ?>&nbsp;р</td>
+                                        <td class="order">
+                                            <div class="popup-zakaz">Заказать</div>
+                                        </td>
+                                    </tr>
+                                <?php endif; ?>
                             <?php endforeach; ?>
                         </tbody>
                     </table>
                 </div>
             <?php endif; ?>
             <div class="clear"></div>
-
         </div>
         <div>
             <div class="prochi-uslugi">
@@ -73,5 +82,28 @@
             </div>
             <div class="clear"></div>
         </div>
+        <?php if (!empty($neis)): ?>
+            <div>
+                <div class="prochi-uslugi">
+                    <h3 id="show-services"><?= $brandPage ? 'Показать цены по прочим неисправностям' : 'Цены по прочим неисправностям'; ?></h3>
+                    <table id="services-table" class="pricelist"<?= $brandPage ? ' style="display: none;"' : ''; ?>>
+                        <tbody>                        
+                            <?php foreach ($neis as $nei): ?>
+                                <tr>
+                                    <td class="name">
+                                        <span class="price-color"><?= $nei['title']; ?></span>                                        
+                                    </td>
+                                    <td class="price"><?= $nei['price'] == 0 ? 'Бесплатно*' : 'от ' . round($nei['price']) . '&nbsp;р'; ?></td>
+                                    <td class="order">
+                                        <div class="popup-zakaz">Заказать</div>
+                                    </td>
+                                </tr>
+                            <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+                <div class="clear"></div>
+            </div>
+        <?php endif; ?>
     </div>
 </section>
