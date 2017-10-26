19cded64e56ac158486d50672551c19eee097d59
Dilshod <br.dilshod@gmail.com>
2017-10-26 11:25:08 +0500

    werwer

--- frontend/views/contact/index.php
+++ frontend/views/contact/index.php
@@ -194,7 +194,7 @@ $siteConfig = app\components\CController::getSiteConfig();
                     </p>
                     <div class="map">
                         <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A80a5761082e8feb488ee844ffe181aed36c78bbb421ef3139be059a805401e2e&amp;width=100%25&amp;height=450&amp;lang=ru_RU&amp;scroll=true"></script>
-                    </div>>                    
+                    </div>                  
                 </div>
             <?php endif; ?>
         </div>
