11911e2359948aea3f5a886ddefb6d7817e70e47
Abdulaziz <youngmaster.web@gmail.com>
2017-11-01 16:49:35 +0500

    hsdfgjashdf;lk

--- frontend/views/layouts/main.php
+++ frontend/views/layouts/main.php
@@ -49,7 +49,7 @@ $js = app\components\CController::$js;
         <?php $this->beginBody() ?>
         <?php if ($isHome): ?>
             <div class="bg-all">
-                <?php if (in_array($siteConfig['category_id'], [2, 4, 5])): ?><div id="particles-js"></div><?php endif; ?>
+                <?php if (in_array($siteConfig['category_id'], [2, 5])): ?><div id="particles-js"></div><?php endif; ?>
                 <?php if (in_array($siteConfig['category_id'], [4])): ?>
                     <div id="signals">
                         <span class="signal s1"></span>
