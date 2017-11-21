922f0a8029775b3a266964fe382253be4dc5529f
Dilshod <br.dilshod@gmail.com>
2017-11-21 11:58:35 +0500

    rtyrty

--- frontend/components/MainUrlRule.php
+++ frontend/components/MainUrlRule.php
@@ -91,7 +91,7 @@ class MainUrlRule extends UrlRule {
                 $page['meta_h1'] = !empty($seo['meta_h1']) ? $seo['meta_h1'] : isset($page['meta_h1']) ? $page['meta_h1'] : '';
                 $page['description'] = $seo['meta_text1'] ?: $page['description'];
                 if (isset($siteConfig['multi_category']))
-                    $page['full_description'] = $seo['meta_text2'] ?: $page['full_description'];
+                    $page['full_description'] = $seo['meta_text2'] ?: !empty ($page['full_description']) ? $page['full_description'] : '';
             }
         }
 
