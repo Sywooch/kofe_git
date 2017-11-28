1f080d02706367bf367492aeea066231dc20eff4
Dilshod <br.dilshod@gmail.com>
2017-11-28 10:59:06 +0500

    ert

--- frontend/models/AskForm.php
+++ frontend/models/AskForm.php
@@ -15,6 +15,7 @@ class AskForm extends Model {
     public function rules() {
         return [
             ['phone', 'required'],
+            ['phone', 'match', 'pattern' => '/^\+7\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/', 'message' => ' Что-то не так' ],
         ];
     }
 
--- frontend/models/ServiceForm.php
+++ frontend/models/ServiceForm.php
@@ -16,6 +16,7 @@ class ServiceForm extends Model {
     public function rules() {
         return [
             ['phone', 'required'],
+            ['phone', 'match', 'pattern' => '/^\+7\s\([0-9]{3}\)\s[0-9]{3}\-[0-9]{2}\-[0-9]{2}$/', 'message' => ' Что-то не так' ],
         ];
     }
 
