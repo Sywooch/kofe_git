3e4f8fb6bdcaab2679100e2025ef2efa6feaee6c
Dilshod <br.dilshod@gmail.com>
2017-11-29 16:56:49 +0500

    werwer

--- frontend/themes/kofe03/widgets/forms/views/mainPageForm.php
+++ frontend/themes/kofe03/widgets/forms/views/mainPageForm.php
@@ -40,7 +40,7 @@ $form = ActiveForm::begin([
         </div>
         <span id="messenger"></span>
         
-        <div class="personalData"><?= $form->field($model, 'agree')->checkbox(['checked' => true, 'label' => null])->label(false); ?><span>Согласен с условиями <a href="obrabotka-personalnyh-dannyh/index.html" target="_blank">обработки персональных данных</a></span></div>
+        <div class="personalData"><?= $form->field($model, 'agree')->checkbox(['checked' => true, 'label' => null])->label(false); ?><span>Согласен с условиями <a href="/polzovatelskoe-soglashenie" target="_blank">обработки персональных данных</a></span></div>
     </div>
     <?php ActiveForm::end() ?>
 </div>
