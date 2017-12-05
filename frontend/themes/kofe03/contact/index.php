000fea6a70917a891f138186964f64bd1902f687
Dilshod <br.dilshod@gmail.com>
2017-12-05 15:02:07 +0500

    345345

--- frontend/themes/kofe03/contact/index.php
+++ frontend/themes/kofe03/contact/index.php
@@ -26,11 +26,11 @@ $this->title = !empty($pageInfo['meta_title']) ? $pageInfo['meta_title'] : $page
                         <p>Сервисный центр "Kofe03.ru"</p>
                         <p>Телефон:&nbsp;<a href='tel:+74951350003'>8 (495) 135-00-03</a></p>
                         <ul>
-                            <li>г. Москва, пл. Победы, д. 1А </li>
+                            <li>г. Москва, Большой Кисловский пер., 5 - 7С2</li>
                         </ul>
                         <p>Время работы: 08:00 &ndash; 22:00</p>
                         <div class="post__maps">
-                            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A30af65369578d250a40a2d07c009ccc057a8edb2b2fd99fe638d341eca27f3cd&amp;width=662&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
+                            <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A82f1d81ac409732fa4a65cee20c92773a717ed2644039a9e4b2c1918268e4e73&amp;width=662&amp;height=400&amp;lang=ru_RU&amp;scroll=true"></script>
                         </div>
                     </div>
                 </article>
