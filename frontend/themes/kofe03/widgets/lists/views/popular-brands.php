645c0ae2a71a4b827692bdd47184a7531c40d58a
Dilshod <br.dilshod@gmail.com>
2017-12-05 16:46:29 +0500

    werwer

--- frontend/themes/kofe03/widgets/lists/views/popular-brands.php
+++ frontend/themes/kofe03/widgets/lists/views/popular-brands.php
@@ -9,6 +9,7 @@ $siteConfig = app\components\CController::getSiteConfig();
             <div class="swiper-container brands__slider">
                 <div class="swiper-wrapper brands__list">
                     <?php foreach ($rows as $key => $row): ?>
+                        <img style="opacity: 0;position: absolute;z-index: -999999999;height: 100%;width: 100%;top: 0px;left: 0px;" src="<?= $assets ?>/uploads/images/<?= $row['image']; ?>" alt="">
                         <div class="swiper-slide brands__item">
                             <a class="brands__box" href="/<?= $row['url']; ?>">
                                 <img class="brands__name brands__name_bosch" src="<?= $assets ?>/uploads/images/<?= $row['image']; ?>" alt="<?= $row['title']; ?>">                                
