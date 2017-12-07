df6088e713eee63c4471111b0c1cfb1a55ce06f8
Dilshod <br.dilshod@gmail.com>
2017-12-07 11:53:56 +0500

    werwer

--- frontend/themes/kofe03/widgets/other/views/subways.php
+++ frontend/themes/kofe03/widgets/other/views/subways.php
@@ -3,26 +3,9 @@
         <h3 class="content__subtitle">Обслуживаемые города <b>МО</b></h3>
         <div class="citys">
             <ul>
-                <li><a href="/balashiha">Балашиха</a></li>
-                <li><a href="/himki">Химки</a></li>
-                <li><a href="/podolsk">Подольск</a></li>
-                <li><a href="/korolyov">Королёв</a></li>
-                <li><a href="/lyubercy">Люберцы</a></li>
-                <li><a href="/mytishchi">Мытищи</a></li>
-                <li><a href="/ehlektrostal">Электросталь</a></li>
-                <li><a href="/zheleznodorozhnyj">Железнодорожный</a></li>
-                <li><a href="/kolomna">Коломна</a></li>
-                <li><a href="/odincovo">Одинцово</a></li>
-                <li><a href="/krasnogorsk">Красногорск</a></li>
-                <li><a href="/serpuhov">Серпухов</a></li>
-                <li><a href="/orekhovo-zuevo">Орехово-Зуево	</a></li>
-                <li><a href="/shchyolkovo">Щёлково</a></li>
-                <li><a href="/domodedovo">Домодедово</a></li>
-                <li><a href="/zhukovskij">Жуковский</a></li>
-                <li><a href="/sergiev-posad">Сергиев Посад</a></li>
-                <li><a href="/pushkino">Пушкино</a></li>
-                <li><a href="/ramenskoe">Раменское</a></li>
-                <li><a href="/noginsk">Ногинск</a></li>
+                <?php foreach ($rows as $row): ?>
+                    <li><a href="/<?= $row['url']; ?>"><?= $row['title']; ?></a></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
