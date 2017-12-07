c223d6d6c41c3d3560c58d368bb82baa1a5584e1
Dilshod <br.dilshod@gmail.com>
2017-12-07 11:55:15 +0500

    ertert

--- frontend/themes/kofe03/widgets/other/Subways.php
+++ frontend/themes/kofe03/widgets/other/Subways.php
@@ -7,8 +7,9 @@ use yii\base\Widget;
 class Subways extends Widget {
 
     public function run() {
-        $sql = 'SELECT url, title FROM {{%pages}} WHERE type = \'zone2\' ORDER BY title';
-        $rows = Yii::$app->db->createCommand($sql)->queryAll();
+        $siteConfig = CController::getSiteConfig();
+        $sql = 'SELECT url, title FROM {{%pages}} WHERE type = \'zone2\' AND site_id = ' . $siteConfig['id'] . ' ORDER BY title';
+        $rows = \Yii::$app->db->createCommand($sql)->queryAll();
         return $this->render('subways', ['rows' => $rows]);
     }
 
