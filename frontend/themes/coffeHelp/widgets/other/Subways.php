c875a2ae371793cff9c3db68310f2c40c283aace
Dilshod <br.dilshod@gmail.com>
2017-12-21 11:13:35 +0500

    ertert

--- frontend/themes/coffeHelp/widgets/other/Subways.php
+++ frontend/themes/coffeHelp/widgets/other/Subways.php
@@ -7,8 +7,10 @@ use yii\base\Widget;
 class Subways extends Widget {
 
     public function run() {
-        
-        return $this->render('subways');
+        $siteConfig = \app\components\CController::getSiteConfig();
+        $sql = 'SELECT url, title FROM {{%pages}} WHERE type = \'zone2\' AND site_id = ' . $siteConfig['id'] . ' ORDER BY title';
+        $rows = \Yii::$app->db->createCommand($sql)->queryAll();
+        return $this->render('subways', ['rows' => $rows]);
     }
 
 }
--- frontend/themes/coffeHelp/widgets/other/views/subways.php
+++ frontend/themes/coffeHelp/widgets/other/views/subways.php
@@ -3,26 +3,9 @@
         <h3 class="content__subtitle">Обслуживаемые города <b>МО</b></h3>
         <div class="citys">
             <ul>
-                <li><a href="#">Балашиха</a></li>
-                <li><a href="#">Химки</a></li>
-                <li><a href="#">Подольск</a></li>
-                <li><a href="#">Королёв</a></li>
-                <li><a href="#">Люберцы</a></li>
-                <li><a href="#">Мытищи</a></li>
-                <li><a href="#">Электросталь</a></li>
-                <li><a href="#">Железнодорожный</a></li>
-                <li><a href="#">Коломна</a></li>
-                <li><a href="#">Одинцово</a></li>
-                <li><a href="#">Красногорск</a></li>
-                <li><a href="#">Серпухов</a></li>
-                <li><a href="#">Орехово-Зуево	</a></li>
-                <li><a href="#">Щёлково</a></li>
-                <li><a href="#">Домодедово</a></li>
-                <li><a href="#">Жуковский</a></li>
-                <li><a href="#">Сергиев Посад</a></li>
-                <li><a href="#">Пушкино</a></li>
-                <li><a href="#">Раменское</a></li>
-                <li><a href="#">Ногинск</a></li>
+                <?php foreach ($rows as $row): ?>
+                    <li><a href="/<?= $row['url']; ?>"><?= str_replace(['Ремонт кофемашин в городе ', '!'], '', $row['title']); ?></a></li>
+                <?php endforeach; ?>
             </ul>
         </div>
     </div>
