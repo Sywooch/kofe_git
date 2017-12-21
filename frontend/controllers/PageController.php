0ab219bde52778ac1c01b8d44074ccdd8a1a5145
Dilshod <br.dilshod@gmail.com>
2017-12-21 10:58:41 +0500

    456456

--- frontend/controllers/PageController.php
+++ frontend/controllers/PageController.php
@@ -258,8 +258,23 @@ class PageController extends CController {
                 fputcsv($fp, $fields);
             }
             fclose($fp);
-            if (file_exists($path . 'urls.csv')) {
-                return Yii::$app->response->xSendFile($path . 'urls.csv');
+            $file = $path . 'urls.csv';
+            if (file_exists($file)) {
+                if (false !== ($handler = fopen($file, 'r'))) {
+                    header('Content-Description: File Transfer');
+                    header('Content-Type: application/octet-stream');
+                    header('Content-Disposition: attachment; filename=' . basename($file));
+                    header('Content-Transfer-Encoding: binary');
+                    header('Expires: 0');
+                    header('Cache-Control: must-revalidate, post-check=0, pre-check=0');
+                    header('Pragma: public');
+                    header('Content-Length: ' . filesize($file)); //Remove
+                    //Send the content in chunks
+                    while (false !== ($chunk = fread($handler, 4096))) {
+                        echo $chunk;
+                    }
+                }
+                exit;
             }
             exit;
         }
