4667029989dc3b80d20a9d6367d1169ba34bd619
Dilshod <br.dilshod@gmail.com>
2017-12-26 14:16:22 +0500

    345345

--- frontend/components/CController.php
+++ frontend/components/CController.php
@@ -159,7 +159,7 @@ class CController extends \yii\web\Controller {
                 'ip' => $userIP,
                 'site' => Yii::$app->request->hostInfo,
             ])->execute();
-            file_get_contents(urlencode('https://mobi03.ru/kofeOrders?phone=' . $phone . '&title=' . $title . '&url=' . Yii::$app->request->hostInfo . Yii::$app->request->pathInfo));
+            file_get_contents('https://mobi03.ru/kofeOrders?phone=' . urlencode($phone) . '&title=' . urlencode($title) . '&url=' . Yii::$app->request->hostInfo . Yii::$app->request->pathInfo);
         }
         $msg = "Телефон: " . $phone;
         $msg .= "\r\nСтраница: " . $title;
