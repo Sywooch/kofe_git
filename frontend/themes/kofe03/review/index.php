4c885eb5f8fa8c697cb0a542ff0f4cdf86b7b994
Abdulaziz <youngmaster.web@gmail.com>
2018-01-05 18:27:43 +0500

    ыфв

--- frontend/themes/kofe03/review/index.php
+++ frontend/themes/kofe03/review/index.php
@@ -55,6 +55,49 @@ $this->title = !empty($pageInfo['meta_title']) ? $pageInfo['meta_title'] : $page
                </div>
             </div>
             <?php endforeach; ?>
+            <div class="post__content">
+              <br>
+              <h2>Оставит отзыв</h2>
+               <form class="form form_feedback ajax_form" id="feedBack" method="post" action="form/" novalidate="novalidate">
+                  <input type="hidden" name="nospam:blank" value="">
+                  <div class="form__row">
+                     <label class="form__element form__element_input">
+                     <input class="form__input form__input_big" name="name" placeholder="Ваше имя">
+                     </label>
+                     <label class="form__element form__element_input">
+                        <input class="form__input form__input_big" type="text" name="order" placeholder="Номер договора">
+                        <span class="form__help">
+                           <svg class="form__help-ico" xmlns="http://www.w3.org/2000/svg" width="9" height="18" viewBox="0 0 9 18">
+                              <path class="form__help-path" d="M1976.82,1413a2.23,2.23,0,1,1,2.25-2.23A2.145,2.145,0,0,1,1976.82,1413Zm3.03-10.07a4.3,4.3,0,0,0-1.28,3.06v0.44h-3.32l-0.03-.63a5.252,5.252,0,0,1,1.48-3.82,4.7,4.7,0,0,0,1.45-2.57c0-.86-0.58-1.45-1.83-1.47a4.791,4.791,0,0,0-2.47.73l-0.85-2.66a8.7,8.7,0,0,1,4.17-1.01c3.3,0,4.83,1.79,4.83,3.82C1982,1400.68,1980.8,1401.91,1979.85,1402.93Z" transform="translate(-1973 -1395)"></path>
+                           </svg>
+                           <span class="form__example">
+                           <img class="form__order" src="images/example.png" alt="">
+                           </span>
+                        </span>
+                     </label>
+                     <label class="form__element form__element_input form__element_valid">
+                     <input class="form__input form__input_big form__input_phone-mask" name="phone" placeholder="Достоинства" required="" aria-required="true">
+                     </label>
+                  </div>
+                  <div class="form__row">
+                     <label class="form__element form__element_textarea">
+                     <textarea class="form__textarea form__textarea_big" name="message" placeholder="Содержание отзыва"></textarea>
+                     </label>
+                  </div>
+                  <div class="clear"></div>
+                  <div class="personalData">
+                     <label class="form__element form__element_input form__element_valid">
+                     <input type="checkbox" name="personalData" checked="checked">
+                     <span class="f14" style="font-size: 10px;">Согласен с условиями <a href="#" target="_blank">обработки персональных данных</a></span></label>
+                  </div>
+                  <label class="form__element form__element_input form__element_valid">
+                     <div class="form__row form__row_submit">
+                        <input type="submit" class="button button_success button_text button_big" name="submit4" value="Отправить">
+                     </div>
+                  </label>
+                  <input type="hidden" name="af_action" value="e7cdd32bc687446376388437b383983f">
+               </form>
+            </div>
         </div>
     </section>
     <?= kofe03\widgets\other\Feedback::widget(); ?>
