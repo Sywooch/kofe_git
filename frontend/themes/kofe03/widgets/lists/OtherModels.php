f177abbc07434f793763ea807e6d239908bb1bf1
Dilshod <br.dilshod@gmail.com>
2018-01-09 15:38:08 +0500

    werwer

--- frontend/themes/kofe03/widgets/lists/OtherModels.php
+++ frontend/themes/kofe03/widgets/lists/OtherModels.php
@@ -11,7 +11,7 @@ class OtherModels extends Widget {
     public $model;
 
     public function run() {
-        $sql = 'select title, url, image from {{%pages}} {{%pages}} where type = \'model\' and active = 1 and category_id = ' . CController::$category['id'] . ' and id != ' . (int) $model['id'] . ' and parent = ' . (int) $model['parent'] . ' order by sort limit 30';
+        $sql = 'select title, url, image from {{%pages}} {{%pages}} where type = \'model\' and active = 1 and category_id = ' . CController::$category['id'] . ' and id != ' . (int) $this->model['id'] . ' and parent = ' . (int) $this->model['parent'] . ' order by sort limit 30';
         $rows = \Yii::$app->db->createCommand($sql)->queryAll();
         return $this->render('otherModels', ['rows' => $rows]);
     }
