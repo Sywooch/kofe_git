530711bce3c4e6a10747f6a5c0e703ea836b3d44
Dilshod <br.dilshod@gmail.com>
2018-01-12 10:47:47 +0500

    werwer

--- frontend/themes/ifixme/layouts/main.php
+++ frontend/themes/ifixme/layouts/main.php
@@ -135,7 +135,7 @@ $js = app\components\CController::$js;
                                     <div class="img"><img src="<?= $assets ?>/ifixme/images/contact.jpg" alt=""></div>
                                     <div class="text">
                                         <p>Москва, ул. Заречная 1к2. М. Фили, Шелепиха</p>
-                                        <a href="tel:+74951520707">+7 (495) 152 07 07</a>
+                                        <a href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= Yii::$app->session['region']['phone']; ?></a>
                                         <span>Ежедневно с 08:00 до 22:00</span>
                                     </div>
                                 </div>
