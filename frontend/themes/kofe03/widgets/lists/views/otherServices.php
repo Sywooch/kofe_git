b2aeb16d20dd800f0a61b8bcb78427918e47f713
Dilshod <br.dilshod@gmail.com>
2018-01-16 15:31:45 +0500

    ertert

--- frontend/themes/kofe03/widgets/lists/views/otherServices.php
+++ frontend/themes/kofe03/widgets/lists/views/otherServices.php
@@ -1,10 +1,11 @@
+<?php $prefUrl = isset($_GET['data']['type']) && in_array($_GET['data']['type'], ['brand', 'model']) ? $_GET['data']['url'] : ''; ?>
 <aside>
     <div class="brands__carousel services-slider">
         <div class="swiper-container services__slider">
             <div class="swiper-wrapper services__list">
                 <?php foreach ($services as $service): ?>
                     <div class="swiper-slide">
-                        <a class="popular__box" href="/ne-delaet-penu">
+                        <a class="popular__box" href="/<?= !empty($prefUrl) ? $prefUrl . '/' : ''; ?><?= $service['url']; ?>">
                             <div class="popular__cover" style="background-image: url(/kofe03/images/services/<?= $service['image']; ?>);"></div>
                             <div class="popular__text">
                                 <p class="popular__name"><?= $service['title']; ?></p>
