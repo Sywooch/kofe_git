0703a656c09c86a55eb4b608f90b727c8b8c8ac3
Dilshod <br.dilshod@gmail.com>
2018-01-22 17:47:14 +0500

    werwerwer

--- frontend/themes/coffeHelp/layouts/main.php
+++ frontend/themes/coffeHelp/layouts/main.php
@@ -24,7 +24,7 @@ $js = app\components\CController::$js;
         <?= Html::csrfMetaTags() ?>
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
-        <link rel="icon" href="favicon.ico" type="image/x-icon">
+        <link rel="icon" href="/favicon.ico" type="image/x-icon">
         <link rel="stylesheet" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/main.css?v=1">
         <link rel="stylesheet" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/main2.css?v=1">
         <script src="<?= $assets . $siteConfig['theme'] . '/'; ?>js/main.js?v=1" async></script>
