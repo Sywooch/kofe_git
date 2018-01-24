541a8b7f00d9879cceee1212b8e12f3365dc7bad
Abdulaziz <youngmaster.web@gmail.com>
2018-01-24 14:38:04 +0500

    5641

--- frontend/themes/coffeHelp/widgets/lists/views/lastReviews.php
+++ frontend/themes/coffeHelp/widgets/lists/views/lastReviews.php
@@ -2,9 +2,8 @@
     <h4>Отзывы</h4>
     <?php foreach ($rows as $row): ?>
         <div class="review_block">
-            <div class="title"><?= $row['username']; ?></div>
+            <div class="title"><b><?= $row['username']; ?></b></div>
             <div class="text"><?= $row['message']; ?></div>
-            <div class="name"><?= date('d.m.Y', strtotime($row['username'])); ?></div>
         </div>
     <?php endforeach; ?>
     <a href="/reviews" class="btn">Еще</a>
