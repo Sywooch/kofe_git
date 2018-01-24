2e83d4ea6ba93a07cc1002b9f4e71916a3f7a670
Dilshod <br.dilshod@gmail.com>
2018-01-24 14:20:36 +0500

    ertertert

--- frontend/themes/coffeHelp/contact/index.php
+++ frontend/themes/coffeHelp/contact/index.php
@@ -17,34 +17,19 @@ $this->title = !empty($pageInfo['meta_title']) ? $pageInfo['meta_title'] : $page
     </section>
     <section class="who who-contacts">
         <div class="row">
-            <div class="col-sm-9 left-col">
-                <h3>Телефон и почта</h3>
+            <div class="col-sm-9 left-col">                
                 <div class="row contacts-container">
                     <div class="col-lg-6 l">
                         <p>Единый справочный телефон</p>
-                        <div class="phone one"> <a href="tel:+74951339049" class="phone-h">+7 (495) 133-90-49</a></div>
-                        <a href="mailto:service@technobit24.ru"><span>service@technobit24.ru</span></a>
+                        <div class="phone one"> <a href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= strip_tags(Yii::$app->session['region']['phone']); ?></a></div>                       
                     </div>
-                    <div class="col-lg-6 r"> Сервисный центр &laquo;Технобыт&raquo;<br/> Работаем с 7:00 до 22:00, без выходных<br/> Наши специалисты выезжают на заказы по всей Москве и до 30 км от МКАД</div>
+                    <div class="col-lg-6 r"><?= $pageInfo['description']; ?></div>
                 </div>
                 <div class="row alert"> <img src="../ui/img/icons/alert-sign.png" alt=""/> Заявки на ремонт принимаются каждый день, без выходных и праздников</div>
                 <?= coffeHelp\widgets\forms\MainPageForm::widget(); ?> 
-                <h3 class="title-mar" style="margin-top:40px;">Выезд мастера</h3>
-                <p>Чтобы сэкономить свое время, вы можете вызвать мастера на дом. Это абсолютно бесплатная услуга. Наш мастер приедет к вам в течение 1 часа. Если ремонт невозможно осуществить на выезде, мы бесплатно доставим вашу технику в сервисный центр. Таким образом, вы получите:</p>
-                <ul class="how_to_order_list">
-                    <li>бесплатный выезд мастера;</li>
-                    <li>бесплатную доставку в сервисный центр;</li>
-                    <li>экономию времени и сил</li>
-                </ul>
-                <h3 class="title-mar">Центральный офис</h3>
-                <p>Основной офис компании находится по адресу: ул. Народного Ополчения, 34, с.2, офис 105. Просим наших клиентов и партнеров заранее сообщать о визите по телефону.</p>
-                <div id="map">
-                    <script type="text/javascript" charset="utf-8" async src="https://api-maps.yandex.ru/services/constructor/1.0/js/?um=constructor%3A151d09ce446dcefca9122e9b82c2071710cff169157caa69dff14efea2cbcc03&amp;width=100%25&amp;height=460&amp;lang=ru_RU&amp;scroll=true"></script>
-                </div>
+                <?= $pageInfo['full_description']; ?>
             </div>
-            
-                <?= coffeHelp\widgets\other\Advantage::widget(['view' => 'advantage3']); ?>
-            
+            <?= coffeHelp\widgets\other\Advantage::widget(['view' => 'advantage3']); ?>
         </div>
     </section>
     <?= coffeHelp\widgets\other\Advantage::widget(['view' => 'advantage4']); ?>    
