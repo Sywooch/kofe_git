e292866eaac15599e6a3a0533bceb77f49c05167
Abdulaziz <youngmaster.web@gmail.com>
2018-01-26 10:57:26 +0500

    gfkv

--- frontend/themes/kofe03/widgets/lists/views/otherServices.php
+++ frontend/themes/kofe03/widgets/lists/views/otherServices.php
@@ -12,14 +12,14 @@ if (!empty($url))
             <div class="swiper-wrapper services__list">
                 <?php foreach ($services as $service): ?>
                     <div class="swiper-slide">
-                        <a class="popular__box" href="/<?= !empty($prefUrl) ? $prefUrl . '/' : ''; ?><?= $service['url']; ?>">
+                        <div class="popular__box">
                             <div class="popular__cover" style="background-image: url(/kofe03/images/services/<?= $service['image']; ?>);"></div>
                             <div class="popular__text">
-                                <p class="popular__name"><?= $service['title']; ?></p>
+                                <a href="/<?= !empty($prefUrl) ? $prefUrl . '/' : ''; ?><?= $service['url']; ?>" class="popular__name"><?= $service['title']; ?></a>
                                 <?= $service['description']; ?>
                                 <p class="popular__price">от <?= number_format($service['price'], 0, ' ', ' '); ?> р.</p>
                             </div>
-                        </a>                   
+                        </div>                   
                     </div>
                 <?php endforeach; ?>
             </div>
