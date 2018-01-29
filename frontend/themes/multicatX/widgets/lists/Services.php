30eaed45ab56097d463b1aec3f7175b1aa676adf
Dilshod <br.dilshod@gmail.com>
2018-01-29 16:23:49 +0500

    rtyrty

--- frontend/themes/multicatX/widgets/lists/Services.php
+++ frontend/themes/multicatX/widgets/lists/Services.php
@@ -17,6 +17,7 @@ class Services extends Widget {
     public function run() {
         $sql = 'SELECT price, title, url, description, is_popular FROM {{%services}} WHERE type = ' . (int) $this->type . '' . ($this->is_popular ? ' and is_popular = 1' : ($this->type == 1 ? ' and is_popular != 1' : '')) . ' and category_id = ' . $this->category_id;
         $rows = \Yii::$app->db->createCommand($sql)->queryAll();
+        CController::seoShuffle($rows, $siteConfig['id']);
         return $this->render('services', ['services' => $rows, 'title' => $this->title, 'urlPrefix' => $this->urlPrefix]);
     }
 
