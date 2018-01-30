65dd4bf196e69ef9bd0df319ebf55930961c68ac
Dilshod <br.dilshod@gmail.com>
2018-01-30 11:59:52 +0500

    456456

--- frontend/themes/coffeHelp/widgets/lists/views/topServices.php
+++ frontend/themes/coffeHelp/widgets/lists/views/topServices.php
@@ -4,7 +4,7 @@ $siteConfig = app\components\CController::getSiteConfig();
 ?>
 <div class="row service3">
     <?php foreach ($services as $service): ?>
-        <a class="col-sm-3 panel-body" style="background-image: url(<?= $assets . $siteConfig['theme'] . '/'; ?>images/services/<?= $service['image']; ?>);" href="/<?= $service['url']; ?>">
+        <a class="col-sm-3 panel-body" style="background-image: url(<?= $assets . ($siteConfig['id'] == 53 ? 'coffeeSupport' : $siteConfig['theme']) . '/'; ?>images/services/<?= $service['image']; ?>);" href="/<?= $service['url']; ?>">
             <div class="bg_line">
                 <p><?= $service['title']; ?></p>
             </div>
