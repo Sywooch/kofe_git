5f74b54fe67b1335b838b4ac3ca4825933594bbc
Abdulaziz <youngmaster.web@gmail.com>
2018-01-31 17:12:38 +0500

    sadd

--- frontend/themes/coffeHelp/layouts/main.php
+++ frontend/themes/coffeHelp/layouts/main.php
@@ -26,12 +26,12 @@ $js = app\components\CController::$js;
         <title><?= Html::encode($this->title) ?></title>
         <?php $this->head() ?>
         <link rel="icon" href="/favicon.ico" type="image/x-icon">
-        <link rel="stylesheet" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/main.css?v=2">
-        <link rel="stylesheet" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/main2.css?v=2">
+        <link rel="stylesheet" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/main.css?v=3">
+        <link rel="stylesheet" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/main2.css?v=3">
         <script src="<?= $assets . $siteConfig['theme'] . '/'; ?>js/main.js?v=3" async></script>
         <link type="text/css" href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/jquery-ui.css" rel="stylesheet" media="all" />
         <?php if ($siteConfig['id'] == 53): ?>
-            <link href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/<?= $siteConfig['id']; ?>.css?v=2" rel="stylesheet">
+            <link href="<?= $assets . $siteConfig['theme'] . '/'; ?>css/<?= $siteConfig['id']; ?>.css?v=3" rel="stylesheet">
         <?php endif; ?>
     </head>
     <body role="document">
