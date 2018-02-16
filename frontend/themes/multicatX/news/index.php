52265e3a80b5e7a5d8b29a78e15fd10d7716d729
Abdulaziz <youngmaster.web@gmail.com>
2018-02-16 17:18:09 +0500

    das

--- frontend/themes/multicatX/news/index.php
+++ frontend/themes/multicatX/news/index.php
@@ -10,26 +10,23 @@ $assets = Yii::getAlias('@web');
     <div class="inner_container">
         <?= multicatX\widgets\other\Breadcrumbs::widget(['data' => $breadcrumbs]); ?>
     </div>    
-    <div class="main_container grey_container">
+    <div class=" grey_container">
         <div class="inner_container" style="overflow: hidden;">
-            <div class="product-category-left-info">
-                <h1 class="category_h1" style="text-align: left;"><?= $model['meta_h1']; ?></h1>
-                <div class="news-all">
-                    <?php foreach ($models as $model): ?>
-                        <a href="/<?= $model['url']; ?>" class="item">
-                            <div class="img"><img src="<?= $assets ?>/uploads/images/<?= $model['image']; ?>" alt=""></div>
-                            <p><?= $model['title']; ?></p>
-                            <span>Подробнее</span>
-                        </a>
-                    <?php endforeach; ?>
-                    <div class="clear"></div>
-                </div>
-                <?=
-                \yii\widgets\LinkPager::widget([
-                    'pagination' => $pagination,
-                ]);
-                ?>
-            </div>            
+            <h1 class="category_h1" style="text-align: left;"><?= $model['meta_h1']; ?></h1>
+            <div class="news-all">
+                <?php foreach ($models as $model): ?>
+                    <a href="/<?= $model['url']; ?>">
+                        <div class="img"><!--<img src="<?= $assets ?>/uploads/images/<?= $model['image']; ?>" alt="">--><img src="<?= $assets ?>/multicatX/uploads/images/1.jpg" alt=""></div>
+                        <p><?= $model['title']; ?></p>
+                    </a>
+                <?php endforeach; ?>
+                <div class="clear"></div>
+            </div>
+            <?=
+            \yii\widgets\LinkPager::widget([
+                'pagination' => $pagination,
+            ]);
+            ?>          
         </div>
     </div>
 </div>
