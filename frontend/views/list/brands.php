be09f6f8a578f7440f38d1ee3c32e4bfb04a235f
Abdulaziz <youngmaster.web@gmail.com>
2018-02-19 18:18:26 +0500

    das dasd

--- frontend/views/list/brands.php
+++ frontend/views/list/brands.php
@@ -65,3 +65,19 @@ $siteConfig = app\components\CController::getSiteConfig();
     </div>
 </section>
 <?php endif; ?>
+<?php if($siteConfig['id'] == 2): ?>
+<noindex>
+    <section class="promo-video">
+        <div class="container">
+            <div class="left">
+                <iframe  src="https://www.youtube.com/embed/M3ebpSpcsro?rel=0" frameborder="0" gesture="media" allow="encrypted-media" allowfullscreen></iframe>
+                <!--<div id="promo-video"></div>-->
+            </div>
+            <div class="right">
+                <div class="title">Здравствуйте</div>
+                <p>Сервисный центр по ремонту кофемашин Spb-Remont-Kofe реализует ремонт кофемашин с 2010 года. За это время мы скоординировали работу инженеров таким образом, что все ремонтные услуги выполняются в течении 24 часов с момента Вашего обращения. Все сотрудники предприятия имеют колосcальный опыт в ремонте кофемашин. Сервисный центр работает как с частными так и с юридическими лицами и государственными учреждениями. На все работы, как и на заменённые комплектующие Вам будет предоставлена гарантия сроком до 1 года. Выбирайте профессионалов, сделаем ремонт качественно и ответим за качество нашей работы!</p>
+            </div>
+        </div>
+    </section>
+<noindex>
+<?php endif; ?>
