d72ab0d7fe47bb488cc36dbbc7d051e12a059488
Dilshod <br.dilshod@gmail.com>
2018-02-20 17:38:31 +0500

    werwer

--- frontend/themes/multicatX/list/service.php
+++ frontend/themes/multicatX/list/service.php
@@ -16,7 +16,13 @@ $this->title = isset($pageInfo['meta_title']) ? $pageInfo['meta_title'] : '';
                 <h1 class="category_h1" style="text-align: left;"><?= !empty($pageInfo['meta_h1']) ? $pageInfo['meta_h1'] : $pageInfo['title'] . ' ' . \app\components\CController::$category['title']; ?></h1>
                 <div class="category-info">
                     <?php if (!empty($pageInfo['description'])): ?>
-                        <?= str_replace('#model_en#', \app\components\CController::$monoBrand['title'] . ' ' . $pageInfo['title'], $pageInfo['description']); ?>
+                        <?php
+                        if (!empty($page) && $page['type'] == 'model')
+                            echo str_replace('#model_en#', \app\components\CController::$monoBrand['title'] . ' ' . $pageInfo['title'], $pageInfo['description']);
+                        elseif (!empty($page) && $page['type'] == 'brand') {
+                            echo str_replace('#brand_en#', \app\components\CController::$monoBrand['title'], $pageInfo['description']);
+                        }
+                        ?>
                     <?php else: ?>
                         <p style="text-align: justify;"><span style="font-family:verdana,geneva,sans-serif;"><span style="font-size:14px;"><span style="color:#808080;">Ремонт <?= \app\components\CController::$monoBrand['title'] . ' ' . mb_strtolower($pageInfo['title'], 'utf8'); ?> предполагает выполнение операций, отличающихся сложностью и способом, который был выбран для восстановления устройства. С какой бы сложностью вы не столкнулись, заниматься самостоятельным ремонтом – задача, не оправдывающая ни затраченных средств, ни времени. К тому же, – это мероприятие может быть убыточным, так как любое неумелое действие способно привести к выходу устройства из строя.<br>
                                         <br>
