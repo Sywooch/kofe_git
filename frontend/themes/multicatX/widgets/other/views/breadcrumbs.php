645b1bb849d0f1d2bded51e227fa9add1a8bceae
Dilshod <br.dilshod@gmail.com>
2018-02-23 15:51:08 +0500

    werwer

--- frontend/themes/multicatX/widgets/other/views/breadcrumbs.php
+++ frontend/themes/multicatX/widgets/other/views/breadcrumbs.php
@@ -1,11 +1,11 @@
 <div itemscope itemtype="http://schema.org/BreadcrumbList" class="breadcrumb">
-    <a itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" href="/">Сервисный центр <?= \app\components\CController::$monoBrand['title']; ?> <meta itemprop="position" content="0" /></a>
+    <a itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" href="/"><span itemscope="" itemtype="http://schema.org/Thing" itemprop="item">Сервисный центр <?= \app\components\CController::$monoBrand['title']; ?></span> <meta itemprop="position" content="0" /></a>
     <?php $a = 1; ?>
     <?php foreach ($data as $url => $title): ?>               
         <?php if (end($data) == $title): ?>
-            <a itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><?= $title; ?><meta itemprop="position" content="<?= $a; ?>" /></a>
+            <a itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem"><span itemscope="" itemtype="http://schema.org/Thing" itemprop="item"><?= $title; ?></span><meta itemprop="position" content="<?= $a; ?>" /></a>
         <?php else: ?>
-            <a itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" href="<?= $url; ?>"><?= $title; ?><meta itemprop="position" content="<?= $a; ?>" /></a>      
+            <a itemprop="itemListElement" itemscope itemtype="http://schema.org/ListItem" href="<?= $url; ?>"><span itemscope="" itemtype="http://schema.org/Thing" itemprop="item"><?= $title; ?></span><meta itemprop="position" content="<?= $a; ?>" /></a>      
             <?php $a++; ?>                    
         <?php endif; ?>                
     <?php endforeach; ?>
