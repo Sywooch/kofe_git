0ff3fd146b3df62022e8eaba95f2a4f3b5cfee6c
Dilshod <br.dilshod@gmail.com>
2018-02-26 12:50:43 +0500

    qweqwe

--- backend/views/seo-templates/_form.php
+++ backend/views/seo-templates/_form.php
@@ -53,7 +53,8 @@ foreach (Yii::$app->params['siteConfigs'] as $title => $config) {
     ?>
 
     <?= $form->field($model, 'template')->textarea(['maxlength' => true]) ?>
-
+<input id="testme" type="button" value="Проверить"><br><br>
+<span id="result"></span>
     <?=
     $form->field($model, 'brand_id')->widget(Select2::classname(), [
         'data' => ['Все бренды'],
@@ -94,6 +95,51 @@ foreach (Yii::$app->params['siteConfigs'] as $title => $config) {
     <?php ActiveForm::end(); ?>
 
 </div>
+<script>
+    function bracketTester(brackets) {
+        if (typeof brackets != "string") {
+            throw new Error("'brackets' is not a string");
+        }
+        if (brackets.length % 2) {
+            throw new Error("'brackets' length is not even");
+        }
+        var table = {};
+        var bracket, i;
+        for (i = 0; i < brackets.length; i++) {
+            if (table.hasOwnProperty(bracket = brackets.charAt(i))) {
+                throw new Error("non-unique character encountered");
+            }
+            table[ bracket ] = {
+                id: i >>> 1,
+                open: !(i % 2)
+            };
+        }
+        return function (string) {
+            if (typeof string != "string") {
+                throw new Error("'string' is not a string");
+            }
+            var stack = [];
+            var bracket, i;
+            for (i = 0; i < string.length; i++) {
+                if (table.hasOwnProperty(bracket = string.charAt(i))) {
+                    if (table[ bracket ].open) {
+                        stack.push(table[ bracket ].id);
+                    } else if (stack.length == 0 || stack.pop() != table[ bracket ].id) {
+                        return false;
+                    }
+                }
+            }
+            return stack.length == 0;
+        }
+    }
+    var test = bracketTester("()[]{}<>");
+    function byId(id) {
+        return document.getElementById(id);
+    }
+    byId("testme").onclick = function () {
+        byId("result").innerHTML = test(byId("seotemplates-template").value) ? "все ок" : "Пропущена закрывающая или открывающая скобка";
+    }
+</script>
 <?php
 $script = 'function getModels(brandID) {
             $.post("/seo-templates/get-models", {
