1a9bc582d13505ce51d2317b1558a00b91e4f214
Dilshod <br.dilshod@gmail.com>
2018-02-28 14:53:52 +0500

    123123

--- frontend/themes/coffeHelp/widgets/lists/views/popular-brands.php
+++ frontend/themes/coffeHelp/widgets/lists/views/popular-brands.php
@@ -5,6 +5,7 @@ $siteConfig = app\components\CController::getSiteConfig();
 <section class="brend my container">
     <h3>Поддерживаем бренды</h3>
     <?php foreach ($rows as $key => $row): ?>
+        <img style="opacity: 0;position: absolute;z-index: -999999999;height: 100%;width: 100%;top: 0px;left: 0px;" src="<?= $assets ?>/uploads/images/<?= $row['image']; ?>" alt="">
         <a href="/<?= $row['url']; ?>" class="brand-page-link"><img src="<?= $assets ?>/uploads/images/<?= $row['image']; ?>" alt="<?= $row['title']; ?>"/></a> 
     <?php endforeach; ?>
     <div class="clear"></div>
