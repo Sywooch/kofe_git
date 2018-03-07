77a4c2673238b03bbad935e6bff373c8a4a5ae35
Dilshod <br.dilshod@gmail.com>
2018-03-07 14:54:36 +0500

    234234

--- frontend/themes/multicat/widgets/lists/views/allPrices.php
+++ frontend/themes/multicat/widgets/lists/views/allPrices.php
@@ -3,30 +3,32 @@
         <div class="title">Сколько стоит?</div>
         <div id="bottons-tablet" class="bottons frame">
             <div class="clearfix">
-                <a onclick="return false;" href="#" data-tab="tabs-1" class="active">Популярные услуги</a>
-                <a onclick="return false;" href="#" data-tab="tabs-2">Остальные услуги</a>
+                <?php if (!empty($services)): ?><a onclick="return false;" href="#" data-tab="tabs-1" class="active">Популярные услуги</a><?php endif; ?>
+                <a onclick="return false;" href="#" data-tab="tabs-2"<?= empty($services) ? ' class="active"' : ''; ?>>Остальные услуги</a>
             </div>
         </div>
-        <div id="tabs-1" class="tab-content active">
-            <table>
-                <tr class="head">
-                    <th>Тип ремонта</th>
-                    <th>Стоимость</th>
-                    <th>Время на ремонт</th>
-                </tr>
-                <?php foreach ($services as $service): ?>
-                    <tr>
-                        <td><a href="/<?= $parent['url'] . '/' . $service['url']; ?>"><?= $service['title']; ?></a> <?= !empty($service['description']) ? '<span>' . $service['description'] . '</span>' : ''; ?></td>
-                        <td><?= number_format((!empty($service['model_price']) ? $service['model_price'] : $service['price']), 0, ' ', ' '); ?> руб.</td> 
-                        <td>
-                            <time><?= $service['time']; ?></time>
-                            <a class="btn green out-icon open-popup" data-tab="popup2" onclick="return false;" href="#">ЗАКАЗАТЬ СЕЙЧАС</a>
-                        </td>
+        <?php if (!empty($services)): ?>
+            <div id="tabs-1" class="tab-content active">
+                <table>
+                    <tr class="head">
+                        <th>Тип ремонта</th>
+                        <th>Стоимость</th>
+                        <th>Время на ремонт</th>
                     </tr>
-                <?php endforeach; ?>
-            </table>
-        </div>
-        <div id="tabs-2" class="tab-content">
+                    <?php foreach ($services as $service): ?>
+                        <tr>
+                            <td><a href="/<?= $parent['url'] . '/' . $service['url']; ?>"><?= $service['title']; ?></a> <?= !empty($service['description']) ? '<span>' . $service['description'] . '</span>' : ''; ?></td>
+                            <td><?= number_format((!empty($service['model_price']) ? $service['model_price'] : $service['price']), 0, ' ', ' '); ?> руб.</td> 
+                            <td>
+                                <time><?= $service['time']; ?></time>
+                                <a class="btn green out-icon open-popup" data-tab="popup2" onclick="return false;" href="#">ЗАКАЗАТЬ СЕЙЧАС</a>
+                            </td>
+                        </tr>
+                    <?php endforeach; ?>
+                </table>
+            </div>
+        <?php endif; ?>
+        <div id="tabs-2" class="tab-content<?= empty($services) ? ' active' : ''; ?>">
             <table>
                 <tr class="head">
                     <th>Тип ремонта</th>
