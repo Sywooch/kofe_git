864b247a17cbf937c632b3169daa1ba003f2f940
Dilshod <br.dilshod@gmail.com>
2018-03-22 12:46:02 +0500

    redirects

--- common/config/redirects.php
+++ common/config/redirects.php
@@ -2,5 +2,50 @@
 
 return [
     50 => ['http://kofe03.ru/remont-kofemashin-c3' => 'http://kofe03.ru'],
-    53 => ['http://coffee-support.ru/uslugi-i-ceny' => 'http://coffee-support.ru/price', 'http://coffee-support.ru/brendy' => 'http://coffee-support.ru/brands'],
+    53 => [
+        'http://coffee-support.ru/uslugi-i-ceny' => 'http://coffee-support.ru/price',
+        'http://coffee-support.ru/brendy' => 'http://coffee-support.ru/brands'
+    ],
+    124 => [
+        'http://kofemashin-remont.ru/repair-delonghi/esam-3500/kompleksnaya-profilaktika' => 'http://kofemashin-remont.ru/repair-delonghi',
+        'http://kofemashin-remont.ru/repair-saeco/aroma-sm-400/remont-kapuchinatora' => 'http://kofemashin-remont.ru/repair-saeco',
+        'http://kofemashin-remont.ru/repair-jura/impressa-c9-one-touch/chistka-ot-kofeinykh-masel' => 'http://kofemashin-remont.ru/repair-jura',
+        'http://kofemashin-remont.ru/repair-jura/giga-x7c-professional/remont-nasosa' => 'http://kofemashin-remont.ru/repair-jura',
+        'http://kofemashin-remont.ru/repair-saeco/hd-8762/zamena-modulya-upravleniya' => 'http://kofemashin-remont.ru/repair-saeco',
+        'http://kofemashin-remont.ru/repair-saeco/hd-8664/zamena-modulya-upravleniya' => 'http://kofemashin-remont.ru/repair-saeco',
+        'http://kofemashin-remont.ru/repair-saeco/odea-giro-v2/dostavka-kofemashiny' => 'http://kofemashin-remont.ru/repair-saeco',
+        'http://kofemashin-remont.ru/repair-krups/121-aromacafe/dostavka-kofemashiny' => 'http://kofemashin-remont.ru/repair-krups',
+        'http://kofemashin-remont.ru/repair-gaggia/new-baby/ne-molet-kofe' => 'http://kofemashin-remont.ru/repair-gaggia',
+        'http://kofemashin-remont.ru/repair-jura/impressa-j9-4-one-touch-tft/remont-nasosa' => 'http://kofemashin-remont.ru/repair-jura',
+        'http://kofemashin-remont.ru/repair-krups/nespresso-umat-xn2505/zamena-uplotnitelnogo-koltsa-gruppy' => 'http://kofemashin-remont.ru/repair-krups',
+        'http://kofemashin-remont.ru/repair-jura/e8-chrome/remont-kofemolki' => 'http://kofemashin-remont.ru/repair-jura',
+        'http://kofemashin-remont.ru/repair-saeco/hd-8953/zamena-mikrovyklyuchatelei' => 'http://kofemashin-remont.ru/repair-saeco',
+        'http://kofemashin-remont.ru/repair-animo/mt100/ne-delaet-kofe' => 'http://kofemashin-remont.ru',
+        'http://kofemashin-remont.ru/repair-siemens/te806501de/remont-kapuchinatora' => 'http://kofemashin-remont.ru/repair-siemens',
+        'http://kofemashin-remont.ru/repair-smalvic/remont-gidrosistemy' => 'http://kofemashin-remont.ru/repair-bork',
+        'http://kofemashin-remont.ru/repair-siemens/tc-40203/kompleksnaya-profilaktika' => 'http://kofemashin-remont.ru/repair-siemens',
+        'http://kofemashin-remont.ru/repair-siemens/tc-24001/chistka-ot-kofeinykh-masel' => 'http://kofemashin-remont.ru/repair-siemens',
+        'http://kofemashin-remont.ru/repair-delonghi/dc-300-digit/remont-gidrosistemy' => 'http://kofemashin-remont.ru/repair-delonghi',
+        'http://kofemashin-remont.ru/repair-saeco/hd-8927/zamena-modulya-upravleniya' => 'http://kofemashin-remont.ru/repair-saeco',
+        'http://kofemashin-remont.ru/repair-delonghi/esam-4300/protekaet' => 'http://kofemashin-remont.ru/repair-delonghi',
+        'http://kofemashin-remont.ru/repair-foster/zamena-mikrovyklyuchatelei' => 'http://kofemashin-remont.ru/repair-bork',
+        'http://kofemashin-remont.ru/repair-reneka/viva-e-1/dostavka-kofemashiny' => 'http://kofemashin-remont.ru/',
+        'http://kofemashin-remont.ru/repair-delonghi/es-156-v/zamena-mikrovyklyuchatelei' => 'http://kofemashin-remont.ru/repair-delonghi',
+        'http://kofemashin-remont.ru/repair-marco/filtro-flask' => 'http://kofemashin-remont.ru/',
+        'http://kofemashin-remont.ru/repair-jura/impressa-c9-one-touch/chistka-ot-kofeinykh-masel' => 'http://kofemashin-remont.ru/repair-jura/impressa-c9-one-touch',
+        'http://kofemashin-remont.ru/repair-animo/excelso-t/dostavka-kofemashiny' => 'http://kofemashin-remont.ru/repair-gino'
+    ],
+    51 => [
+        'https://coffee-help.ru/remont-delonghi/nespresso-umilk/dekofenatsiya' => 'https://coffee-help.ru/remont-delonghi',
+        'https://coffee-help.ru/remont-bosch/tes-70621-rw/ne-vklyuchaetsya' => 'https://coffee-help.ru/remont-bosch',
+        'https://coffee-help.ru/remont-gaggia/babila/net-para' => 'https://coffee-help.ru',
+        'https://coffee-help.ru/remont-gaggia/paros/diagnostics' => 'https://coffee-help.ru',
+        'https://coffee-help.ru/remont-delonghi/nespresso-umilk/dekofenatsiya' => 'https://coffee-help.ru/remont-delonghi/nespresso-umilk',
+        'https://coffee-help.ru/remont-gaggia/babila/net-para' => 'https://coffee-help.ru/remont-krups',
+        'https://coffee-help.ru/remont-sanremo/opera-3-gr/ne-nalivaet-kofe' => 'https://coffee-help.ru',
+        'https://coffee-help.ru/remont-gaggia/babila/net-para' => 'https://coffee-help.ru/remont-gaggia',
+        'https://coffee-help.ru/remont-bauknecht/kmt-9145-ixl/shipping' => 'https://coffee-help.ru',
+        'https://coffee-help.ru/remont-delonghi/icm-65-t/ne-greet-vodu' => 'https://coffee-help.ru/remont-delonghi',
+        'https://coffee-help.ru/remont-severin/ka-4770/zamena-zhernovov' => 'https://coffee-help.ru'
+    ]
 ];
