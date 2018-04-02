ff47a1b70e29e8bf4549b28778ce92738392a3d7
Abdulaziz <youngmaster.web@gmail.com>
2018-04-02 14:33:17 +0500

    das

--- frontend/themes/remont_coffee/widgets/lists/views/randomServices.php
+++ frontend/themes/remont_coffee/widgets/lists/views/randomServices.php
@@ -7,7 +7,9 @@
 		    <?php foreach ($rows as $key => $row): ?>
 		        <li class="works">
 		            <a href="/<?= $row['url']; ?>" style="color:#333;">
-		                <img src="/uploads/images/services/<?= $row['image']; ?>" alt="<?= $row['title']; ?>" style="border-radius: 3px; margin-bottom: 5px">
+		                <div class="img">
+		                	<img src="/uploads/images/services/<?= $row['image']; ?>" alt="<?= $row['title']; ?>" style="border-radius: 3px; margin-bottom: 5px">
+		                </div>
 		                <br>
 		                <span style="border-bottom: dashed 1px #333;"><?= $row['title']; ?></span>
 		            </a>
