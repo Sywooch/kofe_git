1a842d8f2c751e6b86b0f2195b1f9540669261a1
Dilshod <br.dilshod@gmail.com>
2018-04-09 12:07:00 +0500

    werwer

--- frontend/themes/professionals/widgets/other/views/subways.php
+++ frontend/themes/professionals/widgets/other/views/subways.php
@@ -1,5 +1,11 @@
+<?php
+$siteConfig = \app\components\CController::getSiteConfig();
+$r1 = 15;
+if ($siteConfig['id'] == 125)
+    $r1 = 10;
+?>
 <?php foreach ($pages as $key => $page): ?>
-    <?php if (($key % 15) === 0): ?><div class="review"><?php endif; ?>
+    <?php if (($key % $r1) === 0): ?><div class="review"><?php endif; ?>
         <a href="/<?= $page['url']; ?>"> <?= $page['title']; ?></a>
-        <?php if (($key % 15) === 14 || ($key + 1) == count($pages)): ?></div><?php endif; ?>
+        <?php if (($key % $r1) === $r1 - 1 || ($key + 1) == count($pages)): ?></div><?php endif; ?>
 <?php endforeach; ?>
