9afb55da4ed0a63979d40f6f4191c1bbcdd41a09
Dilshod <br.dilshod@gmail.com>
2018-04-10 16:28:22 +0500

    werwer

--- frontend/themes/multicatX/list/category.php
+++ frontend/themes/multicatX/list/category.php
@@ -20,7 +20,11 @@ $breadcrumbs = [
     <?= multicatX\widgets\lists\Services::widget(['title' => 'Цены по услугам:', 'category_id' => CController::$category['id'], 'type' => 1, 'is_popular' => true, 'urlPrefix' => Yii::$app->request->pathInfo . '/']); ?>
     <?= multicatX\widgets\lists\Services::widget(['title' => (CController::$category['id'] == 7 ? 'Цены по прочим услугам:' : 'Цены по услугам:'), 'category_id' => CController::$category['id'], 'type' => 1, 'is_popular' => false]); ?>
     <?= multicatX\widgets\lists\Services::widget(['title' => 'Цены по неисправностям:', 'category_id' => CController::$category['id'], 'type' => 2, 'is_popular' => CController::$category['id'] == 7 ? true : false, 'urlPrefix' => Yii::$app->request->pathInfo . '/']); ?>
-    
+    <div class="inner_container">
+        <div id="content" class="my-text">            
+            <?= str_replace('#brand_en#', CController::$monoBrand['title'], $page['full_description']); ?>
+        </div>
+    </div>
     <?= CController::$category['id'] !=  7 ? multicatX\widgets\lists\Models::widget(['models' => $models, 'view' => 'models2', 'h2' => (!empty($page['meta_h1']) ? $page['meta_h1'] : $page['title']) . ' которые мы чиним:']) : ''; ?>
     <?= multicatX\widgets\other\HowWeWork::widget(); ?>
 </div>
