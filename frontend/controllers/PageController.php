5e11e17fbafe9f0fb394337f808f846c9a435be5
Dilshod <br.dilshod@gmail.com>
2018-04-11 17:36:11 +0500

    qweqwe

--- frontend/controllers/PageController.php
+++ frontend/controllers/PageController.php
@@ -372,7 +372,7 @@ class PageController extends CController {
                     yu_pages m
                 LEFT JOIN yu_pages c ON c.id = m.parent
                 WHERE
-                    m.active = 1 AND c.active = 1 AND m.type != \'brand\' AND c.parent = ' . (int) $siteConfig['brand-id'] . ' AND c.id = m.parent AND c.category_id = 7
+                    m.active = 1 AND c.active = 1 AND m.type != \'brand\' AND c.parent = ' . (int) $siteConfig['brand-id'] . ' AND c.id = m.parent #AND c.category_id = 7
                 AND m.url != \'/\'
                 UNION
                 SELECT
