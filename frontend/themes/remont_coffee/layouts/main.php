9bde2c5a66ce6ccf51b07e2ed584c84481c85bac
Abdulaziz <youngmaster.web@gmail.com>
2018-04-17 17:43:19 +0500

    sd

--- frontend/themes/remont_coffee/layouts/main.php
+++ frontend/themes/remont_coffee/layouts/main.php
@@ -87,7 +87,7 @@ $js = app\components\CController::$js;
                                 <span class="icon-bar"></span>
                                 <span class="icon-bar"></span>
                             </button>
-                            <a class="tel-nav" href="tel:+74950551569">8 (495) 055-15-69</a>
+                            <a class="tel-nav" href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"><?= Yii::$app->session['region']['phone']; ?></a>
                         </div>
                         <div class="collapse navbar-collapse navigation" id="main-menu">
                             <ul id="menu-home" class="nav navbar-nav">
