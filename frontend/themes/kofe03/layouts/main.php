2c5d6eef3c37605b02ac3c717937997c8f1b227c
Abdulaziz <youngmaster.web@gmail.com>
2018-04-20 23:34:44 +0500

    sad

--- frontend/themes/kofe03/layouts/main.php
+++ frontend/themes/kofe03/layouts/main.php
@@ -114,7 +114,7 @@ $js = app\components\CController::$js;
                             <div class="head_b">
                                 <div class="head_br_icon"></div>
                             </div>
-                            <div class="f_mob ph_ico"><a href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"></a></div>
+                            <div class="f_mob ph_ico"><a class="<?= isset($siteConfig['spb']) && $siteConfig['spb'] ? 'telfix' : 'tel03' ?>" href="tel:<?= preg_replace("/\D/", "", Yii::$app->session['region']['phone']); ?>"></a></div>
                             <div class="m_addtn">
                                 <a class="m_btn btn_war popup_js" data-popup="request" href="#">
                                     <img class="b_icon" src="<?= $assets . $siteConfig['theme'] . '/'; ?>images/ico_bellfa5f.svg?v=1485518639601" alt="">
@@ -163,12 +163,12 @@ $js = app\components\CController::$js;
                 </div>
                 <?= $siteConfig['id'] == 52 ? '
                     <div class="top_m_mobile">
-                        <a class="<?= isset($siteConfig["spb"]) && $siteConfig["spb"] ? "telfix" : "tel03" ?>" href="tel:88125012002">8 (812) 501-20-02</a>
+                        <a class="telfix" href="tel:88125012002">8 (812) 501-20-02</a>
                         <span class="navigation_t">Звоните ежедневно с 9:00 до 21:00 м. Сенная,<br> м. Спасская, Набережная канала Грибоедова, д. 59</span>
                     </div>
                 ' : '
                     <div class="top_m_mobile">
-                        <a class="<?= isset($siteConfig["spb"]) && $siteConfig["spb"] ? "telfix" : "tel03" ?>" href="tel:84951350003">8 (495) 135-00-03</a>
+                        <a class="tel03" href="tel:84951350003">8 (495) 135-00-03</a>
                         <span class="navigation_t">Звоните ежедневно с 9:00 до 21:00<br>м. Кутузовская. ул. Студенческая, д. 35</span>
                     </div>
                 '; ?>
