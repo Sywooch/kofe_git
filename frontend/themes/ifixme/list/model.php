1839e5ed3a00524af7521152b4d8b7a66f3dff05
Unknown <br.dilshod@gmail.com>
2018-04-24 13:51:01 +0500

    sdfsdf

--- frontend/themes/ifixme/list/model.php
+++ frontend/themes/ifixme/list/model.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $breadcrumbs = [
     '/' . $brand['url'] => $brand['full_title'],
     $pageInfo['title'],
@@ -9,7 +9,7 @@ $this->title = !empty($pageInfo['meta_title']) ? $pageInfo['meta_title'] : $titl
 <section id="number-5" style="background-image: url(/ifixme/images/upload/<?= strtolower(str_replace('Ремонт ', '', $brand['title'])); ?>.png);">
     <div class="container">
         <h1><?= !empty($pageInfo['meta_h1']) ? $pageInfo['meta_h1'] : $pageInfo['title']; ?></h1>
-        <?= !empty($pageInfo['description']) ? $pageInfo['description'] : (!empty($seoText['text1']) ? $seoText['text1'] : '') ; ?>        
+        <?= !empty($pageInfo['description']) ? $pageInfo['description'] : (!empty($seoText['text1']) ? $seoText['text1'] : ''); ?>        
         <a href="#" class="btn green open-popup" data-tab="popup2" onclick="return false;">Записаться на ремонт</a>
         <div class="clear"></div>
     </div>
@@ -24,14 +24,16 @@ $this->title = !empty($pageInfo['meta_title']) ? $pageInfo['meta_title'] : $titl
         <span class="clear"></span>
     </div>
 </section>
-<section id="number-15">
-    <div class="container">
-        <?= !empty($pageInfo['full_description']) ? $pageInfo['full_description'] : (!empty($seoText['text2']) ? $seoText['text2'] : '') ; ?>
-    </div>
-    <div class="bottom-btn">
-        <span>Развернуть описание</span>
-    </div>
-</section>
+<?php if (!empty($pageInfo['full_description'])): ?>
+    <section id="number-15">
+        <div class="container">
+            <?= $pageInfo['full_description']; ?>
+        </div>
+        <div class="bottom-btn">
+            <span>Развернуть описание</span>
+        </div>
+    </section>
+<?php endif; ?>
 <section id="number-23">
     <a href="#" id="gotop"><i class="fa fa-angle-up" aria-hidden="true"></i></a>
 </section>
--- frontend/themes/ifixme/widgets/lists/views/allPrices.php
+++ frontend/themes/ifixme/widgets/lists/views/allPrices.php
@@ -1,42 +1,25 @@
 <section id="number-10">
     <div class="container">
         <div class="title">Сколько стоит?</div>
-        <div id="bottons-tablet" class="bottons frame">
-            <div class="clearfix">
-                <?php $k = 0; ?>
-                <?php foreach ($models as $key => $model): ?>
-                    <?php if ($model['icon'] == 1 || 1 == 1): ?>            
-                        <a onclick="return false;" href="#" data-tab="tabs-<?= $model['id']; ?>"<?= $k == 0 ? ' class="active"' : '' ?>><?= str_replace($parent['title'] . ' ', '', $model['title']); ?></a>
-                        <?php $k++; ?>
-                    <?php endif; ?>
-                <?php endforeach; ?>                
-            </div>
+        <?php $services = frontend\models\Pages::getModelServices($parent['id']); ?>
+        <div id="tabs-1" class="tab-content active">
+            <table>
+                <tr class="head">
+                    <th>Тип ремонта</th>
+                    <th>Стоимость</th> 
+                    <th>Время на ремонт</th>
+                </tr>
+                <?php foreach ($services as $service): ?>
+                    <tr>
+                        <td><?= $service['title']; ?> <?= !empty($service['description']) ? '<span>' . $service['description'] . '</span>' : ''; ?></td>
+                        <td><?= number_format((!empty($service['model_price']) ? $service['model_price'] : $service['price']), 0, ' ', ' '); ?> руб.</td> 
+                        <td>
+                            <time><?= $service['time']; ?></time>
+                            <a class="btn green out-icon open-popup" data-tab="popup2" onclick="return false;" href="#">ЗАКАЗАТЬ СЕЙЧАС</a>
+                        </td>
+                    </tr>
+                <?php endforeach; ?>
+            </table>
         </div>
-        <?php $k = 0; ?>
-        <?php foreach ($models as $key => $model): ?>
-            <?php if ($model['icon'] == 1 || 1 == 1): ?>
-                <?php $services = frontend\models\Pages::getModelServices($parent['id']); ?>
-                <div id="tabs-<?= $model['id']; ?>" class="tab-content<?= $k == 0 ? ' active' : '' ?>">
-                    <table>
-                        <tr class="head">
-                            <th>Тип ремонта</th>
-                            <th>Стоимость</th> 
-                            <th>Время на ремонт</th>
-                        </tr>
-                        <?php foreach ($services as $service): ?>
-                            <tr>
-                                <td><?= $service['title']; ?> <?= !empty($service['description']) ? '<span>' . $service['description'] . '</span>' : ''; ?></td>
-                                <td><?= number_format((!empty($service['model_price']) ? $service['model_price'] : $service['price']), 0, ' ', ' '); ?> руб.</td> 
-                                <td>
-                                    <time><?= $service['time']; ?></time>
-                                    <a class="btn green out-icon open-popup" data-tab="popup2" onclick="return false;" href="#">ЗАКАЗАТЬ СЕЙЧАС</a>
-                                </td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </table>
-                </div>
-                <?php $k++; ?>
-            <?php endif; ?>
-        <?php endforeach; ?>
     </div>
 </section>
