6769dd07569eed280c30adac88a5870193ce6871
Abdulaziz <youngmaster.web@gmail.com>
2018-04-25 11:36:51 +0500

    dassss

--- frontend/themes/ifixme/widgets/lists/views/models.php
+++ frontend/themes/ifixme/widgets/lists/views/models.php
@@ -3,7 +3,7 @@ $assets = Yii::getAlias('@web');
 ?>
 <section id="number-9">
     <div class="container">
-        <div class="title">Выберите модель <?= $parent['title']; ?></div>
+        <div class="title">Выберите модель <?= str_ireplace('ремонт ', '', $parent['title']); ?></div>
         <div class="content">
             <?php foreach ($rows as $row): ?>
                 <a href="/<?= $row['url']; ?>"><img src="<?= $assets ?>/ifixme/uploads/images/<?= $row['image']; ?>"><?= $row['title']; ?></a>
