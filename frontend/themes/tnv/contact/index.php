1098c746b19ad2b8ee94d3d443d846c4d1266aa6
Unknown <br.dilshod@gmail.com>
2018-04-26 10:37:52 +0500

    asdasdas

--- frontend/themes/tnv/contact/index.php
+++ frontend/themes/tnv/contact/index.php
@@ -19,16 +19,7 @@ $breadcrumbs = [
         <div class="page float--right col-xs-12 col-md-8">
             <div data-flatr="webpage 11">
                 <h1 class="page--title"><?= $pageInfo['meta_h1']; ?></h1>
-                <div class="richtext ">
-                    <p><span class="call_phone_5"><a href="tel:+74957556789" class="caption moscow">+7&nbsp;(495) 755 67 89</a></span> <a href="tel:88005556789" class="caption russia">8&nbsp;(800) 555 67 89</a> <a href="mailto:rekofe@rekofe.ru" class="caption email">rekofe@rekofe.ru</a> <a href="skype:ntv-plus.com" class="caption skype">ntv‑plus.com</a> <a href="whatsapp://+79163802181" class="caption whatsappviber">+7 (916) 380 21 81</a> &nbsp;<a href="https://t.me/ntvplus_bot" class="caption telegram">ntvplus_bot</a></p>
-                    <div class="message warning">
-                        <p>Telegram, WhatsApp работают только в режиме чата.&nbsp;<br />Для получения более оперативного ответа просьба указывать номер оформленного договора.</p>
-                    </div>
-                    <div data-content="region-map" data-contacts-parent="true" class="region-map mb2" data-contacts-url="/ajax/contacts">
-                        <div class="region-map--box">
-                            <iframe src="https://yandex.ru/map-widget/v1/?um=constructor%3A0ff8bd3ce233081eb21bc5385e1162d99deb9e5cf7531729f8b38d4a44e5b1f0&amp;source=constructor" width="100%" height="370" frameborder="0"></iframe>
-                        </div>
-                    </div>
+                <div class="richtext ">                    
                     <?= $pageInfo['description']; ?>
                 </div>
             </div>
