c289398ab02a877829309fb1255564aee58929bf
Abdulaziz <youngmaster.web@gmail.com>
2018-04-30 18:41:20 +0500

    dsa

--- frontend/themes/multicatX/list/services.php
+++ frontend/themes/multicatX/list/services.php
@@ -6,32 +6,58 @@ $breadcrumbs = [
 ?>
 
 <?= multicatX\widgets\other\Breadcrumbs::widget(['data' => $breadcrumbs]); ?>
-<section id="number-26">
-    <div class="container">
-        <h1><?= $pageInfo['meta_h1']; ?></h1>
-        <?php foreach (app\components\CController::$menu as $key => $category): ?>
-            <div class="content-services<?= $key == 0 ? ' active' : '' ?>">
-                <div class="open-botton">
-                    <p>Услуги <?= str_replace('Ремонт ', '', $category['title']); ?></p>
+
+<div class="main_container grey_container">
+
+    <?php foreach (app\components\CController::$menu as $key => $category): ?>
+
+
+    <div id="rumiservice7" class="inner_container">
+       <div class="rumiservice7_topper">
+          <h1 class="rumiservice7_header"><?= $pageInfo['meta_h1']; ?></h1>
+          <div class="rumiservice7_buttons">
+             <div class="rumiservice7_button colortexthover rumiservice7_button_active" button_id="0">Услуги <?= str_replace('Ремонт ', '', $category['title']); ?></div>
+          </div>
+       </div>
+
+        <?php $services = frontend\models\Pages::getCategoryServices($category['category_id']); ?>
+       <div class="rumiservice7_blocks_container">
+          <div class="rumiservice7_blocks rumiservice7_block0" style="display:  block;">
+             <div class="rumiservice7_table_header">
+                <div class="rumiservice_column_1">Наименование позиции</div>
+                <div class="rumiservice_column_2">Стоимость</div>
+                <div class="rumiservice_column_3">Время</div>
+                <div class="rumiservice_column_4"></div>
+             </div>
+             <div class="rumiservice7_table_content">
+                <div class="rumiservice7_table_tr">
+                   <div class="rumiservice_column_1">
+                      <a class="colortexthover" href="/remont-kofemashin/dekaltsinatsiya">Декальцинация</a>
+                   </div>
+                   <div class="rumiservice_column_2">от 750 р.</div>
+                   <div class="rumiservice_column_3">от 2 часов</div>
+                   <div class="rumiservice_column_4"><span class="has_problem mini-form"><span class="colortexthover">Заказать</span></span></div>
                 </div>
-                <?php $services = frontend\models\Pages::getCategoryServices($category['category_id']); ?>
-                <table class="my-table">
-                    <tbody>
-                        <?php foreach ($services as $service): ?>
-                            <tr>
-                                <td>
-                                    <?php if ($service['is_popular'] == 1 && 2 == 1): ?>
-                                        <a href="/<?= $category['url'] . '-' . $service['url']; ?>"><?= $service['title']; ?></a>
-                                    <?php else: ?>
-                                        <?= $service['title']; ?>
-                                    <?php endif; ?>
-                                </td>
-                                <td><span><?= number_format((!empty($service['model_price']) ? $service['model_price'] : $service['price']), 0, ' ', ' '); ?> р</span><a class="btn out-icon open-popup" data-tab="popup2" onclick="return false;" href="#">Заказать</a></td>
-                            </tr>
-                        <?php endforeach; ?>
-                    </tbody>
-                </table>
-            </div>
-        <?php endforeach; ?>
+
+                <?php foreach ($services as $service): ?>
+                    <div class="rumiservice7_table_tr">
+                       <div class="rumiservice_column_1">
+                        <?php if ($service['is_popular'] == 1 && 2 == 1): ?>
+                          <a class="colortexthover" href="/remont-kofemashin/dekaltsinatsiya">Декальцинация</a>
+
+                          <?php else: ?>
+                              <?= $service['title']; ?>
+                        <?php endif; ?>
+                       </div>
+                       <div class="rumiservice_column_2"><?= number_format((!empty($service['model_price']) ? $service['model_price'] : $service['price']), 0, ' ', ' '); ?> р.</div>
+                       <div class="rumiservice_column_3">от 2 часов</div>
+                       <div class="rumiservice_column_4"><span class="has_problem mini-form"><span class="colortexthover">Заказать</span></span></div>
+                    </div>
+                <?php endforeach; ?>
+             </div>
+          </div>
+       </div>
     </div>
-</section>
+    <?php endforeach; ?>
+
+</div>
