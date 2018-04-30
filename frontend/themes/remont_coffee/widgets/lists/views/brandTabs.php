1236ac5b458ee230005d894c88d29c7e04f4452f
Dilshod <br.dilshod@gmail.com>
2018-04-30 16:32:09 +0500

    sdfsdf

--- frontend/themes/remont_coffee/widgets/lists/views/brandTabs.php
+++ frontend/themes/remont_coffee/widgets/lists/views/brandTabs.php
@@ -6,7 +6,7 @@
             <?php endif; ?>
             <div class="tab-b active" rel="tab-neispravnost">Неисправности бренда</div>
             <div class="tab-b" rel="tab-tseni">Цены на услуги</div>
-            <div class="tab-b" rel="tab-text-2"<?= !empty($error['description']) ? ' style="display: none;"' : ''; ?>>Коды ошибок</div>
+            <div class="tab-b" rel="tab-text-2"<?= empty($error['description']) ? ' style="display: none;"' : ''; ?>>Коды ошибок</div>
         </div>
     </div>
     <?= remont_coffee\widgets\lists\Neispravnost::widget(['title' => 'Типовые  <span>неисправности</span> ' . $pageInfo['title']]); ?>
