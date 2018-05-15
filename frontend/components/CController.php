5e1605bc16d2b55841d60e428d7bc7a2846cb4b0
Dilshod <br.dilshod@gmail.com>
2018-05-15 11:53:38 +0500

    zxc

--- frontend/components/CController.php
+++ frontend/components/CController.php
@@ -99,7 +99,7 @@ class CController extends \yii\web\Controller {
 //        }
         
         if(!empty(self::$monoBrand['title']) && isset(Yii::$app->params['brandRussianNames'][self::$monoBrand['title']]))
-            $_GET['data']['meta_title'] = str_replace('#brand_ru#', Yii::$app->params['brandRussianNames'][self::$monoBrand['title']], $_GET['data']['meta_title']);
+            $_GET['data']['meta_title'] = str_replace(['#brand_ru#', '#brand_en#'], [Yii::$app->params['brandRussianNames'][self::$monoBrand['title']], self::$monoBrand['title']], $_GET['data']['meta_title']);
         
         if (empty(Yii::$app->session['region']))
             $this->setRegion(1);
