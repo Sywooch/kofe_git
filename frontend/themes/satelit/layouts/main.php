f0760e4775761aa8993daa6178674e224d2d8126
Abdulaziz <youngmaster.web@gmail.com>
2018-05-24 17:49:23 +0500

    asd

--- frontend/themes/satelit/layouts/main.php
+++ frontend/themes/satelit/layouts/main.php
@@ -28,7 +28,7 @@ $js = app\components\CController::$js;
         <script src="//yastatic.net/es5-shims/0.0.2/es5-shims.min.js"></script>
         <script src="//yastatic.net/share2/share.js"></script>
     </head>
-    <body>
+    <body class="<?= $siteConfig['sitePrefix']; ?>">
         <?php $this->beginBody() ?>
         <?php
         if (!Yii::$app->user->isGuest) {
