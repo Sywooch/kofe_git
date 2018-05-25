d6f62f4e96f9111abe55e60efce2617199584c2e
Dilshod <br.dilshod@gmail.com>
2018-05-25 14:46:49 +0500

    qwe

--- frontend/themes/satelit/widgets/lists/LastReviews.php
+++ frontend/themes/satelit/widgets/lists/LastReviews.php
@@ -7,11 +7,11 @@ use app\components\CController;
 
 class LastReviews extends Widget {
 
-    public $limit = 2;
+    public $limit = 4;
 
     public function run() {
         $siteConfig = CController::getSiteConfig();
-        $q = 'SELECT username, date, rating, message FROM {{%reviews}} WHERE active = 1 ORDER BY date desc LIMIT ' . (int) $this->limit;
+        $q = 'SELECT username, date, rating, message FROM {{%reviews}} WHERE active = 1 AND email = \'' . $siteConfig['sitePrefix'] . '\' ORDER BY date desc LIMIT ' . (int) $this->limit;
         $rows = \Yii::$app->db->createCommand($q)->queryAll();
         if (!empty($rows))
             return $this->render('lastReviews', ['rows' => $rows]);
