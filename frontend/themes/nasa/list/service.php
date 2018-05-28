a64f11a987968375e7d9b88ee3d5ddeaf9850503
Dilshod <br.dilshod@gmail.com>
2018-05-28 15:26:27 +0500

    345

--- frontend/themes/nasa/list/service.php
+++ frontend/themes/nasa/list/service.php
@@ -21,7 +21,7 @@ $siteConfig = app\components\CController::getSiteConfig();
                     <div class="col-md-5">
                         <div class="content__fault">
                             <div class="bg-danger content__fault-icon">
-                                <img alt="" src="/<?= $siteConfig['theme']; ?>/images/services/<?= $pageInfo['image']; ?>.png"> 
+                                <img alt="" src="/<?= $siteConfig['theme']; ?>/img/services/<?= $pageInfo['image']; ?>.png"> 
                             </div> 
                             <div class="content__fault-image"></div>                                
                         </div> 
