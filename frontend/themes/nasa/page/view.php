748496423901ada2c27124d2b21a4ab30626e838
Dilshod <br.dilshod@gmail.com>
2018-05-29 14:58:19 +0500

    qwe

--- frontend/themes/nasa/page/view.php
+++ frontend/themes/nasa/page/view.php
@@ -9,7 +9,7 @@ $breadcrumbs = [
     <div class="container">
         <h1><?= !empty($model['meta_h1']) ? $model['meta_h1'] : $model['title'] ?></h1>
         <div class="row">
-            <div class="col-lg-6 col-md-12">
+            <div class="col-md-12">
                 <?= str_replace('#domain#', '<a class="politica" href="' . Yii::$app->request->hostInfo . '">' . str_replace(['http://', '/'], '', Yii::$app->request->hostInfo) . '</a>', $model['description']); ?>                
             </div>
         </div><br>        
