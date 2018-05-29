589b1aef22f795ea85e712d8c5ea8dcc509a42cf
Dilshod <br.dilshod@gmail.com>
2018-05-29 14:56:30 +0500

    wer

--- frontend/themes/nasa/page/view.php
+++ frontend/themes/nasa/page/view.php
@@ -1,16 +1,20 @@
 <?php
-$this->title = $model['meta_title'];
-$siteConfig = app\components\CController::getSiteConfig();
 $breadcrumbs = [
-    $model['title'],
+    $pageInfo['title'],
 ];
+$this->title = $pageInfo['meta_title'];
+$siteConfig = app\components\CController::getSiteConfig();
 ?>
-<div class="container">
+<div class="content">
     <div class="container">
-        <?= helper\widgets\other\Breadcrumbs::widget(['data' => $breadcrumbs]); ?>
+        <h1><?= !empty($model['meta_h1']) ? $model['meta_h1'] : $model['title'] ?></h1>
+        <div class="row">
+            <div class="col-lg-6 col-md-12">
+                <?= str_replace('#domain#', '<a class="politica" href="' . Yii::$app->request->hostInfo . '">' . str_replace(['http://', '/'], '', Yii::$app->request->hostInfo) . '</a>', $model['description']); ?>                
+            </div>
+        </div><br>        
     </div>
-    <section class="page page-contacts">
-        <h1 class="section-title"><?= !empty($model['meta_h1']) ? $model['meta_h1'] : $model['title'] ?></h1>
-        <?= str_replace('#domain#', '<a class="politica" href="' . Yii::$app->request->hostInfo . '">' . str_replace(['http://', '/'], '', Yii::$app->request->hostInfo) . '</a>', $model['description']); ?>        
-    </section>
 </div>
+<?= nasa\widgets\other\Advantage::widget(); ?>
+<?= nasa\widgets\other\Advantage::widget(['view' => 'warranty']); ?>
+<?= nasa\widgets\other\Advantage::widget(['view' => 'timer']); ?>
