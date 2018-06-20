f54fefdb8c55a95a5f0c2d3d59b2af7d60b8e8a3
Dilshod <br.dilshod@gmail.com>
2018-06-20 14:13:45 +0500

    wer

--- frontend/web/helper/js/template.js
+++ frontend/web/helper/js/template.js
@@ -5410,7 +5410,7 @@ $(function(){
 	/*--*/
 	
 	/*toggle hidden content*/
-	$('.js-content-toggle').on('click',function(e){
+	$(".js-content-toggle").on('click',function(e){
 		e.preventDefault();
 		$(this).parent().toggleClass('content-show');
 		if($(this).parent().hasClass('content-show')){
