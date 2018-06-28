7db4e06258d2e24891c014cd350509d7c8d08465
Dilshod <br.dilshod@gmail.com>
2018-06-28 13:27:49 +0500

    werwer

--- frontend/components/CController.php
+++ frontend/components/CController.php
@@ -31,6 +31,7 @@ class CController extends \yii\web\Controller {
         'helper' => ['name' => 'MSKS4', 'OID' => 2207167051000],        
         'nasa' => ['name' => 'MSKS5', 'OID' => 2207167052000],
         'ofitsial' => ['name' => 'MSKS6', 'OID' => 2207167059000],
+        'landing' => ['name' => 'MSKM7', 'OID' => 2207167017000],
     ];
 
     public static function replaceJS($js) {
