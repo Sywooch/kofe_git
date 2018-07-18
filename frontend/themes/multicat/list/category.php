02c6f39bf03474331f0cf65e553eacb1a4a1c974
Dilshod <br.dilshod@gmail.com>
2018-07-18 15:03:03 +0500

    rtyrty

--- frontend/themes/multicat/list/category.php
+++ frontend/themes/multicat/list/category.php
@@ -3,7 +3,7 @@ $assets = Yii::getAlias('@web');
 $breadcrumbs = [    
     'Ремонт ' . mb_strtolower($page['full_title'], 'utf8'),
 ];
-$this->title = !empty($page['meta_title']) ? $page['meta_title'] : $title;
+$this->title = !empty($page['meta_title']) ? str_replace('#brand_en#', app\components\CController::$monoBrand['title'], $page['meta_title']) : $title;
 
 if(in_array(\app\components\CController::$category['id'], [7, 8])) {
     $bImage = \app\components\CController::$monoBrand['url'] . '_' . \app\components\CController::$category['url'] . '.jpg';
@@ -14,7 +14,7 @@ if(in_array(\app\components\CController::$category['id'], [7, 8])) {
 <?= multicat\widgets\other\Breadcrumbs::widget(['data' => $breadcrumbs]); ?>
 <section id="number-5" style="background-image: url(<?= $assets ?>/multicat/images/upload/<?= $bImage; ?>)">
     <div class="container">        
-        <h1><?= !empty($page['meta_h1']) ? $page['meta_h1'] : 'Ремонт ' . mb_strtolower($page['full_title'], 'UTF-8') . ' ' . app\components\CController::$monoBrand['title']; ?></h1>
+        <h1><?= !empty($page['meta_h1']) ? str_replace('#brand_en#', app\components\CController::$monoBrand['title'], $page['meta_h1']) : 'Ремонт ' . mb_strtolower($page['full_title'], 'UTF-8') . ' ' . app\components\CController::$monoBrand['title']; ?></h1>
         <?= str_replace('#brand_en#', app\components\CController::$monoBrand['title'], $page['description']); ?>
         <div class="clear"></div>
     </div>
