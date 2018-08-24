f9081ef6ab7887a92b4805aa6d648bc1621b53a3
youngmaster <youngmaster.web@gmail.com>
2018-08-24 17:29:22 +0500

    sad

--- frontend/themes/coffee_repair/list/services.php
+++ frontend/themes/coffee_repair/list/services.php
@@ -3,3 +3,7 @@ $assets = '/' . Yii::getAlias('@web');
 $siteConfig = app\components\CController::getSiteConfig();
 $this->title = !empty($pageInfo['meta_title']) ? $pageInfo['meta_title'] : $pageInfo['title'];
 ?>
+<div class="servicessss">
+	<?= coffee_repair\widgets\lists\Neispravnost::widget(['title' => 'Частые неисправности кофемашин', 'type' => 2, 'is_popular' => 1]); ?>
+</div>
+<?= coffee_repair\widgets\lists\Neispravnost::widget(['type' => 1, 'is_popular' => 1, 'view' => 'services']); ?>
